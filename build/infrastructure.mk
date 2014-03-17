7d67e48986d2a24528eed1b949c17bb014882580
John Dilley <john.dilley@citrix.com>
2014-03-17 14:34:13 +0000

    Add zope.interface

--- build/infrastructure.mk
+++ build/infrastructure.mk
@@ -83,6 +83,7 @@ extrapackages-install:
 	$(SUDO) easy_install --upgrade flup
 	$(SUDO) easy_install --upgrade paramiko
 	$(SUDO) easy_install --upgrade uwsgi
+	$(SUDO) easy_install --upgrade zope.interface
 
 
 	$(SUDO) ln -sf `which genisoimage` /usr/bin/mkisofs

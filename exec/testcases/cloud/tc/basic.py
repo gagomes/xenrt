decd506867ac7e6198ef230d675f0ad53c70c90a
John Dilley <john.dilley@citrix.com>
2014-03-19 17:40:35 +0000

    Make the TC clearer

--- exec/testcases/cloud/tc/basic.py
+++ exec/testcases/cloud/tc/basic.py
@@ -3,6 +3,6 @@ import xenrt
 class TCGuestDeploy(xenrt.TestCase):
     def run(self, arglist = []):
         cloud = self.getDefaultToolstack()
-        cloud.createInstance(distro="debian70_x86-64", name="debian")
+        instance = cloud.createInstance(distro="debian70_x86-64", name="debian")
 
         # MarvinApi is cloud.marvin

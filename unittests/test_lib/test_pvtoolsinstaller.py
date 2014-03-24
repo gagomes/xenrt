e3f3b1ff0654e3b847d437762059d1f1656bba9b
Chris Harding <chris.harding@citrix.com>
2014-03-24 13:59:18 +0000

    CP-7973: Add legacy pvtools code for Windows
    Refactor the new pv tools code
    Add legacy code
    Add new class to install windows packages

--- unittests/test_lib/test_pvtoolsinstaller.py
+++ unittests/test_lib/test_pvtoolsinstaller.py
@@ -1,21 +1,38 @@
 from mock import PropertyMock, Mock
 from testing import XenRTUnitTestCase
-from xenrt.lib.cloud.pvtoolsinstall import WindowsXenServer
+from xenrt.lib.cloud.pvtoolsinstall import WindowsXenServer, WindowsLegacyXenServer
 from xenrt.lib.opsys import WindowsOS
 
 
 class TestWindowsXenServerLegacyExcludedForPVTools(XenRTUnitTestCase):
     NON_LEGACY_DISTROS = [("win7", True), ("winxp34344", False), 
                         ("dsjsdfk21312", True), ("w2k3", False)]
+    
+    LEGACY_DISTROS = [("win7", False), ("winxp34344", True), 
+                      ("dsjsdfk21312", False), ("w2k3", True)]
 
     def test_LegacyDistrosReturnFalse(self):
         """Given a legacy distro, when it is provided to the tools installer, then say this is not supported"""
-        self.run_for_many(self.NON_LEGACY_DISTROS, self.__testDistroSupported)
+        self.run_for_many(self.NON_LEGACY_DISTROS, self.__testWindowsXenServerDistroSupported)
+    
+    def test_LegacyObjectSupportsLegacyDistro(self):
+        """Given legacy distro, when asking a legacy pvtools installer, then expect only legacy os to be supported"""
+        self.run_for_many(self.LEGACY_DISTROS, self.__testLegacyWindowsXenServerDistroSupported)
 
-    def __testDistroSupported(self, data):
-        distro, expected = data
+    def __setupMocks(self, distro):
         os = Mock(spec=WindowsOS)
         win = Mock()
         type(win).os = PropertyMock(return_value = os)
         type(win).distro = PropertyMock(return_value = distro)
+        return win
+        
+    def __testWindowsXenServerDistroSupported(self, data):
+        distro, expected = data
+        win = self.__setupMocks(distro)
         self.assertEqual(expected, WindowsXenServer.supportedInstaller(None, win))
+
+    def __testLegacyWindowsXenServerDistroSupported(self, data):
+        distro, expected = data
+        win = self.__setupMocks(distro)
+        self.assertEqual(expected, WindowsLegacyXenServer.supportedInstaller(None, win), "OS checked = %s; expected = %s" % (distro,expected))
+

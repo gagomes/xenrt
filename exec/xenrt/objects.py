cc00b2fc4279af06beb75b023c7015de85d3d208
Chetan Krishnappa <chetan.krishnappa@citrix.com>
2014-04-24 17:50:52 +0530

    Removing junk

--- exec/xenrt/objects.py
+++ exec/xenrt/objects.py
@@ -5123,9 +5123,6 @@ class GenericHost(GenericPlace):
         f = file(filename, "w")
         f.write(ay)
         f.close()
-        xenrt.TEC().logverbose("Printing the autoyast1")
-        xenrt.TEC().logverbose("%s"%ay)
-
         
         # Make autoyast file available over HTTP.
         webdir = xenrt.WebDirectory()
@@ -7604,8 +7601,6 @@ class GenericGuest(GenericPlace):
         ay=ks.generate()
         filename = "%s/autoyast.xml" % (xenrt.TEC().getLogdir())
         f=file(filename,"w")
-        xenrt.TEC().logverbose("Printing the autoyast")
-        xenrt.TEC().logverbose("%s"%ay)
         for line in ay.splitlines():
             f.write("%s\n" % (line))           
         f.close()

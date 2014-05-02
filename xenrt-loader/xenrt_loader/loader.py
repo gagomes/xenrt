a8e198648f00d8068216160bf0418fd39d4e7a72
Mate Lakat <mate.lakat@citrix.com>
2014-05-02 17:27:45 +0100

    [CP-8246] xenrt-loader: use xenrt.in as xenrt.ctrl
    The makefile's responsibility was to generate xenrt.ctrl from xenrt.in.
    To be able to load the module without touching the filesystem, the
    module is created in-memory and imported to the actual runtime.

--- xenrt-loader/xenrt_loader/loader.py
+++ xenrt-loader/xenrt_loader/loader.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import imp
 
 _xenrt_root = None
 
@@ -14,6 +15,17 @@ def get_xenrt_root():
 def load_xenrt(xenrt_root):
     sys.path.append(os.path.join(xenrt_root, 'exec'))
     sys.path.append(os.path.join(xenrt_root, 'lib'))
-    sys.modules['xenrt.ctrl'] = object()
+    xenrt_in_path = os.path.join(xenrt_root, 'control', 'xenrt.in')
+
+    with open(xenrt_in_path, 'rb') as xenrt_in_file:
+        xenrt_in_code = xenrt_in_file.read()
+
+    xenrt_ctrl_module = imp.new_module(xenrt_in_path)
+    exec(xenrt_in_code, xenrt_ctrl_module.__dict__)
+
+    sys.modules['xenrt.ctrl'] = xenrt_ctrl_module
+
+    import xenrt
+    xenrt.ctrl = xenrt_ctrl_module
     global _xenrt_root
     _xenrt_root = xenrt_root

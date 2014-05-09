22a9f820fe24ed82c42588a4c165532a70df65b9
Chetan Krishnappa <chetan.krishnappa@citrix.com>
2014-05-09 14:33:34 +0530

    Adding test_opsys_sles.py

---
+++ unittests/test_lib/test_opsys_sles.py
@@ -0,0 +1,39 @@
+from testing import XenRTUnitTestCase, interfaceMock
+import xenrt
+from xenrt.lib.opsys.sles import SLESLinux 
+
+class TestSLESLinuxOverrides(XenRTUnitTestCase):
+    
+    def setUp(self):
+        self.parent = interfaceMock(xenrt.interfaces.OSParent)
+
+    def testKnownDistroOverride(self):
+        """
+        Given a string representing a distro, When the class is asked if it is supported, 
+        Then expect only RHEL based distros to return true
+
+        """
+        data = { ("sles", True), ("sles11", True), ("centos", False), ("oel", False)} 
+        self.run_for_many(data, self.__testKnownDistroOverride)
+
+    def __testKnownDistroOverride(self, data):
+        distro, exepcted = data
+        self.assertEqual(exepcted, SLESLinux.knownDistro(distro))
+    
+    def testIsoNameImplementation(self):
+        """Given a SLES distro name, When the ISO name is requested, Then expect the name to be converted"""
+
+        data = {("sles10_x86-32", "sles10_x86-32.iso"), 
+                ("sles112_x86-64", "sles112_x86-64.iso"), 
+                ("sles11_x86-32", "sles11_x86-32.iso"), 
+                ("sles102_x86-64", "sles102_x86-64.iso"), 
+                ("hopkins", None)}
+
+        self.run_for_many(data, self.__testIsoNameImplementation)
+
+    def __testIsoNameImplementation(self, data):
+       distro, expectedIso = data
+       dl = SLESLinux(distro, self.parent)
+       self.assertEqual(expectedIso, dl.isoName)
+
+

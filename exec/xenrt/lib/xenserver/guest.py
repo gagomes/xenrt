db60cf6bb85a14ff5a04e0d2ef3e0ca0bc932774
Dan Lamping <daniel.lamping@citrix.com>
2014-05-30 14:48:23 +0100

    Added workaround for CA-136433

--- exec/xenrt/lib/xenserver/guest.py
+++ exec/xenrt/lib/xenserver/guest.py
@@ -1350,6 +1350,12 @@ exit /B 1
                     if extrawait:
                         xenrt.TEC().logverbose("Wait %d seconds just in case XAPI is still settling." % extrawait)
                         xenrt.sleep(extrawait)
+
+                        if xenrt.TEC().lookup("WORKAROUND_CA136433", True, boolean=True):
+                            for i in range(12):
+                                if self.pvDriversUpToDate():
+                                    break
+                                xenrt.sleep(10)
                     return xenrt.RC_OK
                 else:
                     xenrt.TEC().logverbose("Not found PV driver evidence at xenstore path %s" % pvpath)

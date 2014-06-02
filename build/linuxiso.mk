a9976aa32e853fd78c262115890085462387784b
John Dilley <john.dilley@citrix.com>
2014-06-02 10:35:25 +0000

    Add file

---
+++ build/linuxiso.mk
@@ -0,0 +1,20 @@
+#
+# XenRT ISO Makefile
+#
+include build/config.mk
+include build/tools.mk
+
+ISOS	= $(ls $(LINUX_ISOS_INPUTS))
+
+isos:$(LINUX_ISOS_OUTPUTS)
+	$(info Building Linux ISOs...)
+	$(foreach iso,$(ISOS), $(MAKE) $(iso);)
+
+$(LINUX_ISOS_OUTPUTS):
+	$(info Creating linux ISO output directory... \($@\))
+	$(SUDO) mkdir -p $(LINUX_ISO_OUTPUTS)
+	$(SUDO) chown $(USERID):$(GROUPID) $(IMAGEDIR)
+
+.PRECIOUS: $(LINUX_ISOS_OUTPUTS)/%.iso
+$(LINUX_ISOS_OUTPUTS)/%.iso:
+	$(info Building $@...)

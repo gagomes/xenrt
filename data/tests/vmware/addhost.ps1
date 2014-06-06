9b11ac826404bc50bf7248778c04fed55f2300f2
John Dilley <john.dilley@citrix.com>
2014-06-06 08:18:41 +0000

    Add VMWare scripts

---
+++ data/tests/vmware/addhost.ps1
@@ -0,0 +1,30 @@
+Add-PSSnapIn vm*
+
+$vcenter = $args[0]
+$vuser = $args[1]
+$vpassword = $args[2]
+$datacenter = $args[3]
+$cluster = $args[4]
+$hostaddr = $args[5]
+$huser = $args[6]
+$hpassword = $args[7]
+
+Write-Output Connecting to $vcenter
+
+Connect-ViServer -Server $vcenter -User $vuser -Password $vpassword
+
+Write-Output Checking whether Datacenter Exists
+
+if (!(Get-DataCenter $datacenter)) {
+    Write-Output Datacenter does not exist, creating
+    New-DataCenter -Name $datacenter -Location (Get-Folder -NoRecursion)
+}
+
+Write-Output Checking whether Cluster Exists
+if (!(Get-Cluster $cluster)) {
+    Write-Output Cluster does not exist, creating
+    New-Cluster -Name $cluster -Location (Get-DataCenter $datacenter)
+}
+
+Write-Output Adding Host
+Add-VMHost -Name $hostaddr -Location (Get-Cluster $cluster) -Force -User $huser -Password $hpassword

77cc7634ba07a813a6f0d5598f732c19a8f4420e
John Dilley <john.dilley@citrix.com>
2014-06-10 09:20:32 +0000

    Hyper-V fix

--- exec/xenrt/lib/hyperv/host.py
+++ exec/xenrt/lib/hyperv/host.py
@@ -69,6 +69,7 @@ class HyperVHost(xenrt.GenericHost):
             pxe.addEntry("local", boot="local")
         
         pxe.writeIPXEConfig(self.machine, "%s/wininstall/netinstall/%s/winpe/boot.ipxe" % (xenrt.TEC().lookup("LOCALURL"), self.productVersion))
+        pxe.setDefault("local")
         pxe.writeOut(self.machine)
 
         self.machine.powerctl.cycle()

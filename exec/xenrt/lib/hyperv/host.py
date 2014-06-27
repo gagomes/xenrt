759ddd98d2152e504660ffefec4bd12f6f9a904e
John Dilley <john.dilley@citrix.com>
2014-06-27 10:36:11 +0000

    Fix spelling

--- exec/xenrt/lib/hyperv/host.py
+++ exec/xenrt/lib/hyperv/host.py
@@ -138,7 +138,7 @@ class HyperVHost(xenrt.GenericHost):
         driverlist = drivers.split(",")
         for d in driverlist:
             (archive, inf, pci) = d.split(":",2)
-            self.xmlrpcUnpackTarball("%s/%s.tgz" % (xenrt.TEC().lookup("TEST_TARBALL_BASE"), arvchive), "c:\\")
+            self.xmlrpcUnpackTarball("%s/%s.tgz" % (xenrt.TEC().lookup("TEST_TARBALL_BASE"), archive), "c:\\")
             self.devcon("update \"c:\\%s\\%s\" \"%s\"" % (archive, inf, pci))
 
 

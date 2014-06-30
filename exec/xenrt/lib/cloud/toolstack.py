d0e267bd33c517ef8705376f4d91bc9ea7b7dd7a
John Dilley <john.dilley@citrix.com>
2014-06-30 10:42:07 +0000

    More logging

--- exec/xenrt/lib/cloud/toolstack.py
+++ exec/xenrt/lib/cloud/toolstack.py
@@ -812,9 +812,11 @@ class AdvancedNetworkProviderIsolatedWithStaticNAT(AdvancedNetworkProviderIsolat
         # Acquire IP for network
 
         ip = self.cloudstack.cloudApi.associateIpAddress(networkid=self.network)
-        
+      
+        xenrt.TEC().logverbose("Got IP, ID=%s, Address=%s" % (ip.id, ip.ipaddress)
+
         # Setup static NAT to this VM
-        self.cloudstack.cloudApi.enableStaticNat(ipaddressid = ip.id, virtualmachineid=self.instance.toolstackId)
+        self.cloudstack.cloudApi.enableStaticNat(ipaddressid=ip.id, virtualmachineid=self.instance.toolstackId)
         
         self.instance.inboundip = ip.ipaddress
         self.instance.outboundip = ip.ipaddress

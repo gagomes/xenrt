bc09e3c9ab32615ba9b1674ecf91f7940829ee86
John Dilley <john.dilley@citrix.com>
2014-06-30 17:27:23 +0000

    Fix hosts file

--- exec/xenrt/infrastructuresetup.py
+++ exec/xenrt/infrastructuresetup.py
@@ -596,7 +596,9 @@ def buildHostsFile(config,machines,testpeers,sharedhosts):
         f.write("%s xenrt-controller.xenrt.nsec-xenrtcloud\n" % secAddr)
 
     for v in xenrt.TEC().lookup(["NETWORK_CONFIG", "VLANS"], {}).keys():
-        f.write("%s xenrt-controller.xenrt.%s-xenrtcloud\n" % (xenrt.TEC().lookup(["NETWORK_CONFIG", "VLANS", v, "ADDRESS"]), v.lower()))
+        addr = xenrt.TEC().lookup(["NETWORK_CONFIG", "VLANS", v, "ADDRESS"]. None)
+        if addr:
+            f.write("%s xenrt-controller.xenrt.%s-xenrtcloud\n" % (addr, v.lower()))
 
     for machine in machines:
         addr = config.lookup(["HOST_CONFIGS", machine, "HOST_ADDRESS"], None)

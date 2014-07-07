09421f7bec57a7b522ca4040197f2a74a6fdf48e
John Dilley <john.dilley@citrix.com>
2014-07-07 13:19:19 +0000

    Initial OVM support

--- exec/main.py
+++ exec/main.py
@@ -29,7 +29,7 @@ for p in possible_paths:
     if os.path.exists(p):
         sys.path.append(p)
 
-import xenrt, xenrt.lib.cloud, xenrt.lib.xenserver, xenrt.lib.oss, xenrt.lib.xl, xenrt.lib.generic, xenrt.lib.opsys, xenrt.lib.hyperv
+import xenrt, xenrt.lib.cloud, xenrt.lib.xenserver, xenrt.lib.oss, xenrt.lib.xl, xenrt.lib.generic, xenrt.lib.opsys, xenrt.lib.hyperv, xenrt.lib.oraclevm
 try:
     import xenrt.lib.libvirt
     import xenrt.lib.kvm

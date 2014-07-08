6383ae7518be591795ef8ea737766c023d3b7190
John Dilley <john.dilley@citrix.com>
2014-07-08 16:27:56 +0000

    Add initial stress sequence

---
+++ exec/testcases/cloud/tc/stress.py
@@ -0,0 +1,44 @@
+import xenrt
+
+import random
+
+class TCInstanceLifecycleStress(xenrt.TestCase):
+    def prepare(self, arglist):
+        self.args = self.parseArgsKeyValue(arglist)
+
+        cloud = self.getDefaultToolstack()
+        self.instance = cloud.createInstance(distro=self.args['distro'], hypervisorType=self.args.get("hypervisor"), name=self.args.get("instancename"))
+    
+    def run(self, arglist):
+        ops = {"StopStart": "stopStart", "Reboot": "reboot", "Migrate": "migrate", "SnapRevert": "snapRevert", "SnapDelete": "snapDelete"}
+
+        for i in xrange(int(self.args.get("iterations", 400))):
+            op = random.choice(ops.keys())
+            self.runSubcase(ops[op], (), op, "Iter-%d" % i)
+
+    def stopStart(self):
+        self.instance.stop()
+        self.instance.start()
+
+    def reboot(self):
+        self.instance.reboot()
+
+    def migrate(self):
+        migrateToList = self.instance.canMigrateTo
+        if not migrateToList:
+            xenrt.TEC().logverbose("No suiteable machines for migration")
+            return
+        migrateTo = random.choice(migrateToList)
+        self.instance.migrate(migrateTo)
+
+    def snapRevert(self):
+        snapName = xenrt.randomGuestName()
+        self.instance.createSnapshot(snapName)
+        self.instance.revertInstanceToSnapshot(snapName)
+        self.instance.setPowerState(xenrt.PowerState.up)
+        self.instance.deleteSnapshot(snapName)
+
+    def snapDelete(self):
+        snapName = xenrt.randomGuestName()
+        self.instance.createSnapshot(snapName)
+        self.instance.deleteSnapshot(snapName)

8d20da572a443e2f531022635c88f9de75ba16a4
John Dilley <john.dilley@citrix.com>
2014-07-08 13:10:21 +0000

    Add KVM/VMware templates

--- exec/xenrt/lib/cloud/mansvr.py
+++ exec/xenrt/lib/cloud/mansvr.py
@@ -135,6 +135,10 @@ class ManagementServer(object):
             if xenrt.TEC().lookup("MARVIN_BUILTIN_TEMPLATES", False, boolean=True):
                 templateSubsts["http://download.cloud.com/templates/builtin/centos56-x86_64.vhd.bz2"] = \
                         "%s/cloudTemplates/centos56-httpd-64bit.vhd.bz2" % xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP")
+                templateSubsts["http://download.cloud.com/releases/2.2.0/CentOS5.3-x86_64.ova"] = \
+                        "%s/cloudTemplates/centos53-httpd-64bit.ova" % xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP"),
+                templateSubsts["http://download.cloud.com/releases/2.2.0/eec2209b-9875-3c8d-92be-c001bd8a0faf.qcow2.bz2"] = \
+                        "%s/cloudTemplates/centos55-httpd-64bit.qcow2" % xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP"),
                   
 
             for t in templateSubsts.keys():

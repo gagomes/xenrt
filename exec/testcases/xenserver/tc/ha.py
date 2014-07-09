22916cf76e2011887a32f70c09bbcee030e74999
Alex Brett <alex.brett@citrix.com>
2014-07-09 15:22:41 +0100

    CA-140301 Fix pause logic looking for CA-138629

--- exec/testcases/xenserver/tc/ha.py
+++ exec/testcases/xenserver/tc/ha.py
@@ -516,7 +516,7 @@ class _HATest(xenrt.TestCase):
 
         except xenrt.XRTFailure, e:
             # This isn't a failure of the TC
-            if "No space left on device" in e.data:
+            if e.data and "No space left on device" in e.data:
                 self.pause("CA-138629 repro found", email="alex.brett@citrix.com")
             raise xenrt.XRTError(e.reason,e.data)
 

66cc5e20fd2739e981b640095e1a54dce3dfa1b0
John Dilley <john.dilley@citrix.com>
2014-07-09 11:13:43 +0000

    Add more operations to CCP stress

--- exec/testcases/cloud/tc/stress.py
+++ exec/testcases/cloud/tc/stress.py
@@ -6,13 +6,22 @@ class TCInstanceLifecycleStress(xenrt.TestCase):
     def prepare(self, arglist):
         self.args = self.parseArgsKeyValue(arglist)
 
-        cloud = self.getDefaultToolstack()
-        self.instance = cloud.createInstance(distro=self.args['distro'], hypervisorType=self.args.get("hypervisor"), name=self.args.get("instancename"))
+        self.cloud = self.getDefaultToolstack()
+        self.instance = self.cloud.createInstance(distro=self.args['distro'], hypervisorType=self.args.get("hypervisor"), name=self.args.get("instancename"))
         self.instance.createSnapshot("%s-base" % self.instance.name)
         self.getLogsFrom(self.instance)
+
+        self.snapCount = self.args.get("snapcount", 10)
     
     def run(self, arglist):
-        ops = {"StopStart": "stopStart", "Reboot": "reboot", "Migrate": "migrate", "SnapRevert": "snapRevert", "SnapDelete": "snapDelete"}
+        ops = {"StopStart": "stopStart",
+               "Reboot": "reboot",
+               "Migrate": "migrate",
+               "SnapRevert": "snapRevert",
+               "SnapDelete": "snapDelete",
+               "MultiSnapDelete": "multiSnapDelete",
+               "MultiSnapRevert": "multiSnapRevert",
+               "CloneDelete": "cloneDelete"}
 
         for i in xrange(int(self.args.get("iterations", 400))):
             op = random.choice(ops.keys())
@@ -51,3 +60,40 @@ class TCInstanceLifecycleStress(xenrt.TestCase):
         # Revert to base snapshot to prevent snapshot chain getting too long
         self.instance.revertToSnapshot("%s-base" % self.instance.name)
         self.instance.setPowerState(xenrt.PowerState.up)
+
+    def multiSnapRevert(self):
+        
+        snapNames = [xenrt.randomGuestName() for x in range(self.snapCount)]
+        for s in snapNames:
+            self.instance.createSnapshot(s)
+        for s in snapNames:
+            self.instance.revertToSnapshot(s)
+            self.instance.setPowerState(xenrt.PowerState.up)
+        
+        # Revert to base snapshot to prevent snapshot chain getting too long
+        self.instance.revertToSnapshot("%s-base" % self.instance.name)
+        self.instance.setPowerState(xenrt.PowerState.up)
+
+        for s in snapNames:
+            self.instance.deleteSnapshot(s)
+
+    def multiSnapDelete(self):
+        snapNames = [xenrt.randomGuestName() for x in range(self.snapCount)]
+        for s in snapNames:
+            self.instance.createSnapshot(s)
+        for s in snapNames:
+            self.instance.deleteSnapshot(s)
+        
+        self.instance.revertToSnapshot("%s-base" % self.instance.name)
+        self.instance.setPowerState(xenrt.PowerState.up)
+
+    def cloneDelete(self):
+        templateName = xenrt.randomGuestName()
+        self.cloud.createTemplateFromInstance(self.instance, templateName)
+
+        instance2 = self.cloud.createInstanceFromTemplate(templateName)
+        instance2.destroy()
+        templateid = [x.id for x in self.cloud.cloudApi.listTemplates(templatefilter="all", name=templateName) if x.name==templateName][0]
+
+        self.cloud.cloudApi.deleteTemplate(id=templateid)
+

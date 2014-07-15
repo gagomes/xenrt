737753b0f61c9a57700ed0e1901d12517242b27d
Mate Lakat <mate.lakat@citrix.com>
2014-07-15 09:02:29 -0400

    [CP-8946] Fix test discovery issue
    After the test_osLibraries function was decorated with patch, it wasn't
    picked up by the test framework. By performing the patch inside the
    function, this issue could be eliminated.

--- unittests/test_lib/test_interfaces.py
+++ unittests/test_lib/test_interfaces.py
@@ -6,6 +6,7 @@ from zope.interface.verify import verifyObject, verifyClass
 from zope.interface import implementedBy
 from mock import Mock, patch
 
+
 class TestInterfaces(XenRTTestCaseUnitTestCase):
     def test_cloudstackInterface(self):
         """Verify that the CloudStack class implements the Toolstack interface"""
@@ -44,8 +45,8 @@ class TestInterfaces(XenRTTestCaseUnitTestCase):
         # Do the verification
         verifyObject(xenrt.interfaces.OSParent, i)
 
-@patch('xenrt.TEC')
-def test_osLibraries(_tec):
+
+def test_osLibraries():
     """Generate tests for each known OS library"""
 
     def oslib_test(oslib):
@@ -69,11 +70,12 @@ def test_osLibraries(_tec):
             if not i in implementedInterfaces:
                 raise AssertionError("Interface %s not implemented but stated in supportedInstallMethods" % i.__name__)
 
-    for l in xenrt.lib.opsys.oslist:
-        # We use lambda functions here so we can give them a unique description
-        testfn = lambda: oslib_test(l)
-        testfn.description = "Verify the %s class implements its interfaces" % l.__name__
-        yield testfn
-        testfn = lambda: oslib_supportedInstallMethods(l)
-        testfn.description = "Verify the %s class implements interfaces for all supportedInstallMethods" % l.__name__
-        yield testfn
+    with patch('xenrt.TEC'):
+        for l in xenrt.lib.opsys.oslist:
+            # We use lambda functions here so we can give them a unique description
+            testfn = lambda: oslib_test(l)
+            testfn.description = "Verify the %s class implements its interfaces" % l.__name__
+            yield testfn
+            testfn = lambda: oslib_supportedInstallMethods(l)
+            testfn.description = "Verify the %s class implements interfaces for all supportedInstallMethods" % l.__name__
+            yield testfn

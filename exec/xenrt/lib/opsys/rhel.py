069596eadce9a9dd391982869b82f11ebb4a8df9
John Dilley <john.dilley@citrix.com>
2014-07-21 14:32:09 +0000

    Remove hack for RHEL

--- exec/xenrt/lib/opsys/rhel.py
+++ exec/xenrt/lib/opsys/rhel.py
@@ -124,9 +124,6 @@ class RHELBasedLinux(LinuxOS):
         path = "%s/%s" % (xenrt.TEC().lookup("GUESTFILE_BASE_PATH"), installIP)
         filename = "%s/kickstart.stamp" % path
         xenrt.waitForFile(filename, 1800)
-        if self.distro.startswith("rhel7") or self.distro.startswith("centos7") or self.distro.startswith("oel7"):
-            xenrt.TEC().logverbose("Waiting 2 minutes as the kickstart file is accessed multiple times")
-            xenrt.sleep(1200)
 
     def cleanupIsoAnswerfile(self):
         if self.cleanupdir:

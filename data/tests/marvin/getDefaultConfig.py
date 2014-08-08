683c36678aa77d77bad9cc97eb713da1ada5ef84
Doug Clark <doug.clark@citrix.com>
2014-08-08 12:06:33 +0000

    Import test_data from the installed Marvin lib instead of from file location

--- data/tests/marvin/getDefaultConfig.py
+++ data/tests/marvin/getDefaultConfig.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 
-import sys, json
-
-sys.path.append("/root/cloudstack.git/tools/marvin/marvin/config")
-
-import test_data
+import json
+from marvin.config import test_data
 
 print json.dumps(test_data.test_data, indent=2)

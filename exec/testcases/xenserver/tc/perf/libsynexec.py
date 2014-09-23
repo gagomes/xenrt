8b6cc261858c139f17cdb52e069f5b88e29c30be
Marcus Granado <marcus.granado@citrix.com>
2014-09-23 10:32:23 +0100

    Revert "Revert "synexec: Support running on the controller""
    This reverts commit 5de2077d54dc68f4be6dcacdd215bdf891ac2fbd.

--- exec/testcases/xenserver/tc/perf/libsynexec.py
+++ exec/testcases/xenserver/tc/perf/libsynexec.py
@@ -1,4 +1,7 @@
 import string
+import os
+import subprocess
+import time
 import xenrt
 
 def get_if_name_param():
@@ -25,8 +28,9 @@ def initialise_master_in_dom0(host):
 def initialise_slave(slave):
     _initialise(slave, "synexec_slave")
 
-def start_slave(slave, jobid):
-    slave.execguest("/root/synexec_slave -v -s %d >> /tmp/synexec.log &" % jobid)
+def start_slave(slave, jobid, port=None):
+    port = " -p %d" % port if port else ""
+    slave.execguest("/root/synexec_slave -v%s -s %d >> /tmp/synexec.log &" % (port, jobid))
 
 def start_master_in_dom0(host, slaveCommand, jobid, numclients):
     # Disable firewall on DOM0 to allow synexec to communicate
@@ -38,8 +42,50 @@ def start_master_in_dom0(host, slaveCommand, jobid, numclients):
     # Run synexec master
     host.execdom0("/root/synexec_master %s -v -s %d %d /root/synexec.conf 1>/root/synexec_master.log 2>&1" % (get_if_name_param(), jobid, numclients))
 
+def initialise_master_on_controller(jobid):
+    workdir = "/tmp/synexec%d" % jobid
+    os.mkdir(workdir)
+    os.system("wget -O - '%s/synexec.tgz' | tar -xz -C %s" %
+              (xenrt.TEC().lookup("TEST_TARBALL_BASE"), workdir))
+
+    if subprocess.Popen(["uname", "-m"], stdout=subprocess.PIPE).communicate()[0].strip() == "x86_64":
+        os.rename("%s/synexec/synexec_master.x86_64" % workdir,
+                  "%s/synexec_master" % workdir)
+    else:
+        os.rename("%s/synexec/synexec_master.x86_32" % workdir,
+                  "%s/synexec_master" % workdir)
+
+def start_master_on_controller(slaveCommand, jobid, numclients):
+    workdir = "/tmp/synexec%d" % jobid
+
+    # Write synexec master configuration file
+    conffile = "%s/synexec.conf" % workdir
+    out = open(conffile, "w")
+    out.write(slaveCommand)
+    out.close()
+
+    # Run synexec master
+    port = 5165
+    while port < 5300:
+        p = subprocess.Popen("%s/synexec_master -v -s %d -p %d %d %s 1> %s/synexec_master.log 2>&1" %
+                         (workdir, jobid, port, numclients, conffile, workdir), shell=True)
+        time.sleep(1)
+        if "Address already in use" not in get_master_log_on_controller(jobid):
+            return (p, port)
+
+        port += 1
+
+    raise Exception("Could find free port to start master")
+
 def get_slave_log(slave):
     return slave.execguest("cat /tmp/synexec.out")
 
 def get_master_log(host):
     return host.execdom0("cat /root/synexec_master.log")
+
+def get_master_log_on_controller(jobid):
+    workdir = "/tmp/synexec%d" % jobid
+    f = open("%s/synexec_master.log" % workdir, "r")
+    data = f.read()
+    f.close()
+    return data

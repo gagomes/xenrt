a608f4262f1617cf8c3dae8393d1a0ff0e383a4f
Alex Brett <alex.brett@citrix.com>
2014-09-30 17:54:12 +0100

    Revert "Added generic performance test case library code. (xenrt/performance.py)"
    This reverts commit caad3e0016715d7c80574b67125965bd3e85ad48. Currently unable to submit nightly, seemingly due to some sort of multiple inheritance problem...

--- exec/xenrt/__init__.py
+++ exec/xenrt/__init__.py
@@ -335,7 +335,7 @@ def setGec(gec):
 
 #############################################################################
 
-class TestCase(object):
+class TestCase:
     """The definition and implementation of a testcase.
 
     This is the parent class for all testcases."""

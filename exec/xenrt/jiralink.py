616c178d238d5fe68a440a39d2aecf050ae92caf
John Dilley <john.dilley@citrix.com>
2014-10-06 19:36:38 +0000

    Disable cloudstack greps

--- exec/xenrt/jiralink.py
+++ exec/xenrt/jiralink.py
@@ -140,20 +140,20 @@ class JiraLink:
             {'file':"*/guest-console-logs/console*", 'desc':"Kernel Panic",
                 'pattern':r'(?:.*\n){0,4}(?:.*Kernel panic.*)(?:.*\n){0,4}'},
 
-            {'file':"*/cloudstack/management/management-server.log*", 'desc':"Management server Exception logs",
-                'pattern':r'(?:.*\n){0,1}(?:.*Exception\:.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1},
+#            {'file':"*/cloudstack/management/management-server.log*", 'desc':"Management server Exception logs",
+#                'pattern':r'(?:.*\n){0,1}(?:.*Exception\:.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1},
 
-            {'file':"*/cloudstack/management/management-server.log*", 'desc':"Management server Error logs",
-                'pattern':r'(?:.*\n){0,1}(?:.*ERROR.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1},
+#            {'file':"*/cloudstack/management/management-server.log*", 'desc':"Management server Error logs",
+#                'pattern':r'(?:.*\n){0,1}(?:.*ERROR.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1},
 
-            {'file':"*/cloudstack/management/apilog.log*", 'desc':"API logs",
-                'pattern':r'(?:.*\n){0,5}(?:.*exception.+)(?:.*\n){0,5}','type':"CLOUD"},
+#            {'file':"*/cloudstack/management/apilog.log*", 'desc':"API logs",
+#                'pattern':r'(?:.*\n){0,5}(?:.*exception.+)(?:.*\n){0,5}','type':"CLOUD"},
 
             {'file':"*/cloudstack/management/localhost*", 'desc':"Local Host log",
                 'pattern':r'(?:.*\n){0,1}(?:.*SEVERE\:.+)(?:.*\n){0,10}','type':"CLOUD",'startPoint':-8192,'endPoint':-1},
 
-            {'file':"*/cloudstack/agent.log*", 'desc':"Agent logs",
-                'pattern':r'(?:.*\n){0,1}(?:.*Exception\:.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1}
+#            {'file':"*/cloudstack/agent.log*", 'desc':"Agent logs",
+#                'pattern':r'(?:.*\n){0,1}(?:.*Exception\:.+)(?:.*\n){0,4}','type':"CLOUD",'startPoint':-8192,'endPoint':-1}
         ]
         desc = "\n"
         for fp in failurepatterns:

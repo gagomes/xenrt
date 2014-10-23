6c03635de9ad974ce7ad52236e318963acb6bf19
John Dilley <john.dilley@citrix.com>
2014-10-23 13:59:37 +0000

    Add srpervm test

---
+++ exec/testcases/xenserver/tc/srpervm.py
@@ -0,0 +1,43 @@
+# XenRT: Test harness for Xen and the XenServer product family
+#
+# Test cases for issues with lots of SRs.
+#
+# Copyright (c) 2012 Citrix Systems, Inc. All use and distribution of this
+# copyrighted material is governed by and subject to terms and
+# conditions as licensed by Citrix Systems, Inc. All other rights reserved.
+
+import xenrt
+
+class SetupSRs(xenrt.TestCase):
+    def run(self, arglist=[]):
+        args = self.parseArgsKeyValue(arglist)
+
+        linuxLunCount = int(args.get("linuxluns", "10"))
+        windowsLunCount = int(args.get("windowsluns", "10"))
+
+        windowsFilerName = args.get("windowsfiler", None)
+        linuxFilerName = args.get("linuxfiler", None)
+
+        linuxFiler = xenrt.StorageArrayFactory().getStorageArray(xenrt.StorageArrayVendor.NetApp, xenrt.StorageArrayType.iSCSI, specify=linuxFilerName)
+        windowsFiler = xenrt.StorageArrayFactory().getStorageArray(xenrt.StorageArrayVendor.NetApp, xenrt.StorageArrayType.iSCSI, specify=windowsFilerName)
+
+
+        pool = self.getDefaultHost().getPool()
+        [x.enableMultipathing() for x in pool.getHosts()]
+        initiators = dict((x.getName(), {'iqn': x.getIQN()}) for x in pool.getHosts())
+
+        linuxLuns = linuxFiler.provisionLuns(linuxLunCount, 10, initiators)
+        windowsLuns = windowsFiler.provisionLuns(windowsLunCount, 30, initiators)
+
+        i = 0
+        for lun in linuxLuns:
+            sr = xenrt.lib.xenserver.ISCSIStorageRepository(pool.master, "LinuxSR_%d" % i)
+            sr.create(lun, noiqnset=True, subtype="lvm")
+            i+=1
+        i = 0
+        for lun in windowsLuns:
+            sr = xenrt.lib.xenserver.ISCSIStorageRepository(pool.master, "WindowsSR_%d" % i)
+            sr.create(lun, noiqnset=True, subtype="lvm")
+            i+=1
+
+

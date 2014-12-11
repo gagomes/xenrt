a93b54bbdb543ab3deafe945bc0288978849c580
Swaroop Shankar D H <swaroop.shankar@citrix.com>
2014-12-11 21:21:08 +0530

    Inguest upgrade using template method pattern

--- exec/testcases/guestops/upgrade.py
+++ exec/testcases/guestops/upgrade.py
@@ -9,19 +9,28 @@
 #
 
 import sys, string, time, re
+from abc import ABCMeta, abstractmethod
 import xenrt
 
-class TCLinuxUpgrade(xenrt.TestCase):
+class TCLinuxUpgrade(xenrt.TestCase, object):
 
-    def run(self, arglist=None):
+    __metaclass__ = ABCMeta
+
+    def prepare(self, arglist=None):
+        self.urlPrefix  = xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP", "")
         self.args  = self.parseArgsKeyValue(arglist)
         self.guest = self.getGuest(self.args['guest'])
 
-    def upgradelinux(self,cmd):
+    def run(self, arglist=None):
+        self.preconfigureGuest()
+        self.upgradeLinux(self.upgradeCommand())
+        self.checkUpgrade()
+
+    def upgradeLinux(self,cmd):
         retVal = 0
         for i in range (1,20) :
             retVal = self.guest.execguest("%s" %cmd, timeout=7200 , retval = "code")
-            if not retVal : 
+            if not retVal :
                 break
 
         if retVal:
@@ -30,32 +39,52 @@ class TCLinuxUpgrade(xenrt.TestCase):
         xenrt.TEC().comment("Rebooting to complete the upgrade process")
         self.guest.reboot()
 
+    @abstractmethod
+    def preconfigureGuest(self):
+        pass
+
+    @abstractmethod
+    def checkUpgrade(self):
+        pass
+
+    @abstractmethod
+    def upgradeCommand(self):
+        pass
+
 class TCUbuntuUpgrade(TCLinuxUpgrade):
 
     UBUNTU_NAME_MAP = {"ubuntu1404": "trusty",
                        "ubuntu1204": "precise",
                        "ubuntu1004": "lucid" }
 
-    def run(self):
+    def preconfigureGuest(self):
 
         newDistro = self.args['upgradeto']
         ubuntuName = self.UBUNTU_NAME_MAP[newDistro]
-        self.guest.execguest("echo deb %s %s main restricted > /etc/apt/sources.list" % (xenrt.TEC().lookup(["RPM_SOURCE", newDistro, g.arch, "HTTP"]), ubuntuName))
-        self.guest.execguest("echo deb %s %s-updates main restricted >> /etc/apt/sources.list" % (xenrt.TEC().lookup(["RPM_SOURCE", newDistro, g.arch, "HTTP"]), ubuntuName))
+        ubuntuUrl  = xenrt.TEC().lookup(["RPM_SOURCE", newDistro, self.guest.arch, "HTTP"])
+        sourceList = "/etc/apt/sources.list"
+
+        #replace the sources.list
+        self.guest.execguest("echo deb %s %s main restricted > %s" % (ubuntuUrl, ubuntuName,sourceList ))
+        self.guest.execguest("echo deb %s %s-updates main restricted >> %s" % (ubuntuUrl, ubuntuName, sourceList))
+
         self.guest.execguest("apt-get update")
-        cmd = "/bin/echo -e 'Y\n' | apt-get -y --force-yes dist-upgrade"
-        self.upgradelinux(cmd)
+        xenrt.TEC().comment("apt-get update completed")
+
+    def upgradeCommand(self):
+        return "/bin/echo -e 'Y\n' | apt-get -y --force-yes dist-upgrade"
+
+    def checkUpgrade(self):
+        ubuntuRelease = self.guest.execguest("cat /etc/issue.net")
+        if not re.search(r"^Ubuntu 14.04" , ubuntuRelease) : raise xenrt.XRTError("Upgrade was not successful")
 
 class TCCentosUpgrade(TCLinuxUpgrade):
 
-    def run(self):
-        # Set up the base urls required for centos upgrade
-        centOsurl = xenrt.TEC().lookup(["RPM_SOURCE","centos7","x86-64", "HTTP"])
-        urlPrefix  = xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP", "")
+    def preconfigureGuest(self):
+        fastRpm    = "%s/CentOS/mirror.centos.org/Packages/yum-plugin-fastestmirror-1.1.30-30.el6.noarch.rpm" % (self.urlPrefix)
+        yumRpm     = "%s/CentOS/mirror.centos.org/Packages/yum-3.2.29-60.el6.centos.noarch.rpm" % (self.urlPrefix)
         repoPath   = "/etc/yum.repos.d/upgrade.repo"
-        baseUrl    = "%s/CentOS/dev.centos.org" % (urlPrefix)
-        fastRpm    = "%s/CentOS/mirror.centos.org/Packages/yum-plugin-fastestmirror-1.1.30-30.el6.noarch.rpm" % (urlPrefix)
-        yumRpm     = "%s/CentOS/mirror.centos.org/Packages/yum-3.2.29-60.el6.centos.noarch.rpm" % (urlPrefix)
+        baseUrl    = "%s/CentOS/dev.centos.org" % (self.urlPrefix)
 
         # Create the repo file for the upgrade
         self.guest.execguest("echo '[upgrade]' > %s" % (repoPath))
@@ -70,32 +99,33 @@ class TCCentosUpgrade(TCLinuxUpgrade):
         xenrt.TEC().comment("Replaced yum packages")
 
         # Install tools required for preupgrade
-        [self.guest.execguest("yum -y install --assumeyes %s" %tool) for tool in ['preupgrade-assistant-contents','redhat-upgrade-tool','preupgrade-assistant', 'preupg']]
+        [self.guest.execguest("yum -y install --assumeyes %s" %tool) for tool in ['preupgrade-assistant-contents','redhat-upgrade-tool','preupgrade-assistant']]
+        self.guest.execguest("/bin/echo -e 'y\n' | preupg")
         xenrt.TEC().comment("Pre upgrade completed")
 
-        # run the redhat-upgrade-tool to start the upgrade
+    def upgradeCommand(self):
+        centOsurl = xenrt.TEC().lookup(["RPM_SOURCE","centos7","x86-64", "HTTP"])
+        #import the centos key
         self.guest.execguest("rpm --import %s/RPM-GPG-KEY-CentOS-7" %(centOsurl))
-        cmd = "redhat-upgrade-tool --network 7.0 --force --instrepo %s" %(centOsurl)
-        self.upgradelinux(cmd)
+        return "redhat-upgrade-tool --network 7.0 --force --instrepo %s" %(centOsurl)
 
+    def checkUpgrade(self):
         releaseCentos = self.guest.execguest("cat /etc/centos-release")
-        if not re.search("CentOS\s+Linux\s+release\s+7" , releaseCentos) : raise xenrt.XRTError("Upgrade was not successful")
+        if not re.search(r"^CentOS Linux release 7.0" , releaseCentos) : raise xenrt.XRTError("Upgrade was not successful")
 
 class TCOelUpgrade(TCLinuxUpgrade):
 
-    def run(self):
-        urlPrefix  = xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP", "")
-        # Set up the base urls required for oel upgrade
-        baseUrl    = "%s/Oel/public-yum.oracle.com/repo/OracleLinux/OL6/addons/x86_64" % (urlPrefix)
+    def preconfigureGuest(self):
+
+        baseUrl    = "%s/Oel/public-yum.oracle.com/repo/OracleLinux/OL6/addons/x86_64" % (self.urlPrefix)
+        fastRpm    = "%s/CentOS/mirror.centos.org/Packages/yum-plugin-fastestmirror-1.1.30-30.el6.noarch.rpm" % (self.urlPrefix)
+        yumRpm     = "%s/CentOS/mirror.centos.org/Packages/yum-3.2.29-60.el6.centos.noarch.rpm" % (self.urlPrefix)
         repoPath   = "/etc/yum.repos.d/upgrade.repo"
-        fastRpm    = "%s/CentOS/mirror.centos.org/Packages/yum-plugin-fastestmirror-1.1.30-30.el6.noarch.rpm" % (urlPrefix)
-        yumRpm     = "%s/CentOS/mirror.centos.org/Packages/yum-3.2.29-60.el6.centos.noarch.rpm" % (urlPrefix)
-        oel7Url    = xenrt.TEC().lookup(["RPM_SOURCE","oel7","x86-64", "HTTP"])
 
         # Create the repo file for the upgrade
         self.guest.execguest("echo '[upgrade]' > %s" % (repoPath))
-        self.guset.execguest("echo name'=upgrade' >> %s" % (repoPath))
-        self.guset.execguest("echo baseurl'='%s >> %s" % (baseUrl, repoPath))
+        self.guest.execguest("echo name'=upgrade' >> %s" % (repoPath))
+        self.guest.execguest("echo baseurl'='%s >> %s" % (baseUrl, repoPath))
         self.guest.execguest("echo gpgkey'=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle' >> %s" %(repoPath))
         self.guest.execguest("echo gpgcheck'='1 >> %s" % (repoPath))
         self.guest.execguest("echo enabled'='1 >>  %s" % (repoPath))
@@ -106,12 +136,14 @@ class TCOelUpgrade(TCLinuxUpgrade):
         xenrt.TEC().comment("Replaced yum packages")
 
         # Install tools required for preupgrade
-        [self.guest.execguest("yum -y install --assumeyes %s" %tool) for tool in ['openscap','preupgrade-assistant-contents','redhat-upgrade-tool','preupg']]
+        [self.guest.execguest("yum -y install --assumeyes %s" %tool) for tool in ['openscap','preupgrade-assistant-contents','redhat-upgrade-tool']]
+        self.guest.execguest("/bin/echo -e 'y\n' | preupg")
         xenrt.TEC().comment("Pre upgrade completed")
 
-        # run the redhat-upgrade-tool to start the upgrade
-        cmd = "redhat-upgrade-tool --network 7.0 --force --instrepo %s" %(oel7Url)
-        self.upgradelinux(cmd)
+    def upgradeCommand(self):
+        oel7Url    = xenrt.TEC().lookup(["RPM_SOURCE","oel7","x86-64", "HTTP"])
+        return "redhat-upgrade-tool --network 7.0 --force --instrepo %s" %(oel7Url)
 
-        releaseOel = self.guest.execguest("cat /etc/centos-release")
-        if not re.search("Oracle\s+Linux\s+Server\s+release\s+7" , releaseOel) : raise xenrt.XRTError("Upgrade was not successful")
+    def checkUpgrade(self):
+        releaseOel = self.guest.execguest("cat /etc/oracle-release")
+        if not re.search(r"^Oracle Linux Server release 7.0" , releaseOel) : raise xenrt.XRTError("Upgrade was not successful")

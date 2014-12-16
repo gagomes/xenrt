3d28edeb9ab597f90e342cfb6e1fa31be058e29d
John Dilley <john.dilley@citrix.com>
2014-12-16 10:02:59 +0000

    Add job file download

--- server/app/api/files.py
+++ server/app/api/files.py
@@ -3,7 +3,8 @@ from app.api import XenRTAPIPage
 
 import app.utils
 
-import string, shutil, traceback
+import string, shutil, traceback, mimetypes
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound, HTTPInternalServerError
 
 class XenRTDownload(XenRTAPIPage):
     def render(self):
@@ -38,6 +39,36 @@ class XenRTDownload(XenRTAPIPage):
                 return "ERROR File missing"
             else:
                 return "ERROR Internal error"
+
+class XenRTJobFileDownload(XenRTAPIPage):
+    def render(self):
+        prejob = self.request.params.get("prejob") in ("yes", "true")
+
+        details = self.get_job(self.request.matchdict['job'])
+        if prejob:
+            server = details['JOB_FILES_SERVER']
+        else:
+            server = details['LOG_SERVER']
+
+        if server != self.request.host:
+            return HTTPFound(location="http://%s%s" % (server, self.request.path))
+
+        filename = self.request.matchdict[filename]
+        (ctype, encoding) = mimetypes.guess_type(filename)
+        if not ctype:
+            ctype = "application/octet-stream"
+        
+        try:
+            localfilename = app.utils.results_filename(filename, self.request.matchdict['job'])
+            self.request.response.body_file = file(localfilename, "r")
+            self.request.response.content_type=ctype
+            if encoding:
+                self.request.response.content_encoding=encoding
+        except Exception, e:
+            if isinstance(e, IOError):
+                return HTTPNotFound()
+            else:
+                return HTTPInternalServerError()
 
 class XenRTUpload(XenRTAPIPage):
     WRITE = True
@@ -108,4 +139,5 @@ class XenRTUpload(XenRTAPIPage):
         cur.close()
 
 PageFactory(XenRTDownload, "download", "/api/files/download", compatAction="download")
+PageFactory(XenRTJobFileDownload, "jobfiledownload", "/api/getjobfile/{job}/{filename}", compatAction="download")
 PageFactory(XenRTUpload, "upload", "/api/files/upload", compatAction="upload")

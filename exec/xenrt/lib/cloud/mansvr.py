de81e3fc84f5e8ffddda1b3f9f9897b6b24b87ad
Vivek Kumar Chaubey <vivekkumar.chaubey@citrix.com>
2014-12-23 19:51:28 +0530

    fix CCP issue

--- exec/xenrt/lib/cloud/mansvr.py
+++ exec/xenrt/lib/cloud/mansvr.py
@@ -128,7 +128,11 @@ class ManagementServer(object):
                     self.place.execcmd("echo 'proxy=http://%s' >> /etc/yum.conf" % proxy)
                 self.place.execcmd("wget -O mysql-repo.rpm %s/rpms/mysql-community-release-el7-5.noarch.rpm" % xenrt.TEC().lookup("EXPORT_DISTFILES_HTTP"))
                 self.place.execcmd("yum install -y mysql-repo.rpm")
-                self.place.execcmd('yum -y install mysql-server mysql')
+                if xenrt.TEC().lookup("WORKAROUND_CS30447", False, boolean=True):
+                    xenrt.TEC().warning("Using workaround for CS-30447")
+                    self.place.execcmd("yum -y install mysql-community-server-5.6.21 mysql")
+                else:
+                    self.place.execcmd('yum -y install mysql-server mysql')
         self.place.execcmd('service %s restart' % self.db)
         self.place.execcmd('chkconfig %s on' % self.db)
 

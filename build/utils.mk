8e53c02e2a8062ce92227e189e533362a7a1f7c9
Alex Brett <alex.brett@citrix.com>
2015-01-05 11:27:55 +0000

    Add a make target and script to get the current db schema
    Also include the most recent schema in control/database.sql

--- build/utils.mk
+++ build/utils.mk
@@ -9,3 +9,7 @@ $(addprefix $(ROOT)/$(XENRT)/scripts/distutils/,$(UTILSCRIPTS)):
 	  ln -s $(ROOT)/$(INTERNAL)/utils/$(notdir $@) $@ || \
 	  $(ROOT)/$(XENRT)/scripts/distutils/getUtil.py $(notdir $@) $(ROOT)/$(XENRT)/scripts/distutils
 
+.PHONY: dbschema
+dbschema:
+	$(ROOT)/$(XENRT)/scripts/getdbschema $(ROOT)/$(XENRT)/control/database.sql
+	git diff $(ROOT)/$(XENRT)/control/database.sql

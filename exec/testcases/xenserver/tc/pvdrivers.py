b0a0f668d46acf3c347dc1c395cd25b2e90d0b7c
ramakrishp <ramakrishna.padyana@citrix.com>
2015-01-12 21:26:26 +0530

    import statements added

--- exec/testcases/xenserver/tc/pvdrivers.py
+++ exec/testcases/xenserver/tc/pvdrivers.py
@@ -9,7 +9,9 @@
 #
 
 import socket, re, string, time, traceback, sys, random, copy, os.path
+from datetime import datetime
 import xenrt
+from xenrt.lib.xenserver.signedpackages import *
 
 class TC8369(xenrt.TestCase):
     """Verify Windows PV drivers install to a Windows 2008 x64 VM without a test certificate"""

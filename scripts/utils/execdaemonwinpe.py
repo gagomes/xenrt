7859db567e83000c493172417cf54e83548cfe3a
John Dilley <john.dilley@citrix.com>
2015-01-13 12:57:04 +0000

    Add file_exists to winpe execdaemon

--- scripts/utils/execdaemonwinpe.py
+++ scripts/utils/execdaemonwinpe.py
@@ -1,5 +1,6 @@
 import subprocess
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import os.path
 
 class WinPEExec(object):
     def __init__(self):
@@ -18,6 +19,9 @@ class WinPEExec(object):
     def read_file(self, fname):
         with open(fname) as f:
             return f.read()
+
+    def file_exists(self, fname):
+        return os.path.exists(fname)
 
 xmlrpc = SimpleXMLRPCServer(("0.0.0.0", 8080), allow_none=True)
 xmlrpc.register_introspection_functions()

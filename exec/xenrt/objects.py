0807c36d9f92d95b3a580a3fbcedcfa44b7e9be3
Ronan Murphy <ronan.murphy2@citrix.com>
2015-01-20 16:10:32 +0000

    Fixed install commands for lshw rpm

--- exec/xenrt/objects.py
+++ exec/xenrt/objects.py
@@ -9361,9 +9361,9 @@ sleep (3000)
 
         # Install lshw if needed.
         if not isUbuntu:
-            self.execguest("wget -nv '%s/lshw.tgz' -O - | tar -zx -C /tmp" %
+            self.execguest("wget -nv '%slshw.tgz' -O - | tar -zx -C /tmp" %
                                                 (xenrt.TEC().lookup("TEST_TARBALL_BASE")))
-            self.execguest("yum -y install /tmp/lshw-2.17-1.el7.rf.x86_64.rpm")
+            self.execguest("yum -y install /tmp/lshw/lshw-2.17-1.el7.rf.x86_64.rpm")
 
         # Not aware of possible errors, but might need a try block just in case.
         xml = execguest("lshw -xml -c video")

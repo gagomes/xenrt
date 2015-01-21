758be90e5f18cb7f8e85ecf181fb6bb5baaae33c
Ronan Murphy <ronan.murphy2@citrix.com>
2015-01-21 17:33:20 +0000

    Removing temporary test code #2.

--- exec/testcases/xenserver/tc/vgpu.py
+++ exec/testcases/xenserver/tc/vgpu.py
@@ -3898,3 +3898,4 @@ class TCcheckNvidiaDriver(xenrt.TestCase):
 
         if host.execdom0("grep -e 'nvidia: disagrees about version of symbol' -e 'nvidia: Unknown symbol' /var/log/kern.log", retval="code") == 0:
             raise xenrt.XRTFailure("NVIDIA driver is not correctly built for the current host kernel")
+

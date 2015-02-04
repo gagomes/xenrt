d9c02fa2eb7dfb1037b7c99a0a5ce0d643e45cfd
Chris Harding <chris.harding@citrix.com>
2015-02-04 09:51:11 +0000

    Trim trailing whitespace - yuck

--- exec/xenrt/lib/xenserver/objects.py
+++ exec/xenrt/lib/xenserver/objects.py
@@ -13,26 +13,26 @@ class XapiObjectFactory(object):
     """
     def __init__(self):
         self.__XapiObjects = {}
-    
+
     def getObject(self, type):
         """
         Get a stored class, based on a lookup string. The return value will need to be instantiated
-        
+
         @type type: string
         @var type: A string representing the type of the object to be fetched
         @rtype: class
-        @return: A class of the required type or "XapiObject" if the type is not registered. 
+        @return: A class of the required type or "XapiObject" if the type is not registered.
         """
         if self.__XapiObjects.has_key(type):
             return self.__XapiObjects[type]
         else:
             return XapiObject
-    
+
     def registerObject(self, classToRegister):
         """
-        Register a class, along with it's type to ensure the specialisation is return 
+        Register a class, along with it's type to ensure the specialisation is return
         when a lookup is requested
-        
+
         @type type: string
         @var type: name of the type to register
         @type classToRegister: class reference
@@ -63,7 +63,7 @@ class XapiObject(object):
     def getListParam(self, paramName, delimiter=';'):
         """
         Get the params from xapi, in the form of a list of strings - useful for composite fields
-        
+
         @var paramName: the name of a parameter to fetch from the xapidb
         @type paramName: string
         @var delimiter: how the resulting array is delimeted by xapi
@@ -72,11 +72,11 @@ class XapiObject(object):
         @rtype list
         """
         return self.cli.execute("%s-param-get uuid=%s param-name=%s" % (self.type, self.uuid, paramName )).strip().split(delimiter)
-    
+
     def getDictParam(self, paramName, listDelimiter=';', keyDelimiter=':'):
         """
         Get the params from xapi, in the form of a dictionary of strings - useful for composite fields
-        
+
         @var paramName: the name of a parameter to fetch from the xapidb
         @type paramName: string
         @var listDelimiter: how the resulting array is delimited by xapi
@@ -88,21 +88,21 @@ class XapiObject(object):
         """
         params = {}
         listParams = self.getListParam(paramName, listDelimiter)
-        
+
         if not listParams or len(listParams) < 1:
             return params
-        
+
         for p in listParams:
             pair = p.split(keyDelimiter)
             params[pair[0].strip()]=pair[1].strip()
         return params
 
-        
+
     def getObjectParam(self, objType, paramName):
         """
         Get object-model form of a field referenced by the current object. For example if an object contains a ref to another object
         eg. for a vdi type: and sr class can be obtained by: sr = vdi.getObjectParam("sr", "sr-uuid")
-        
+
         @var objType: the objects type to look up in the factory
         @type objType: string
         @var paramName: the parameter that provides the uuid of the target type
@@ -112,21 +112,21 @@ class XapiObject(object):
         """
         uuid = self.cli.execute("%s-param-get  uuid=%s param-name=%s" % (self.type, self.uuid, paramName)).strip()
         return objectFactory().getObject(objType)(self.cli, objType, uuid)
-    
+
     def getObjectsReferencing(self, refObjectType, currentObjectType):
         """
         Get objects that reference the current objecy
         @var refObjectType: the type to look up
         @type refObjectType:  string
-        @var currentObjectType: how the resulting array is refered to  
+        @var currentObjectType: how the resulting array is refered to
         @type delimiter: string
         @return XapiObjects represented the objects that reference the current object
-        @rtype list of XapiObjects 
+        @rtype list of XapiObjects
         """
         uuids = self.cli.execute("%s-list %s-uuid=%s --minimal" % (refObjectType, currentObjectType, self.uuid)).strip().split(',')
         return [objectFactory().getObject(refObjectType)(self.cli, refObjectType, uuid) for uuid in uuids]
-        
-   
+
+
     def op(self, operation, params="", returnObject=None):
         ret = self.cli.execute("%s-%s uuid=%s %s" % (self.type, operation, self.uuid, params)).strip()
         if returnObject:
@@ -135,13 +135,13 @@ class XapiObject(object):
 
     #Setter could be implemented like so....
     #def setParam(paramName, value): etc...
-    
+
 class NamedXapiObject(XapiObject):
     __NAME = "name-label"
-    
+
     def name(self):
         return self.getStringParam(self.__NAME)
-    
+
     def getObjectsReferencingName(self, refObjectType, currentObjectType):
         uuids = self.cli.execute("%s-list %s=%s --minimal" % (refObjectType, currentObjectType, self.name())).strip().split(',')
         return [objectFactory().getObject(refObjectType)(self.cli, refObjectType, uuid) for uuid in uuids]
@@ -153,18 +153,18 @@ NB: Don't forget to register any new implementations with the object factory
 class VM(NamedXapiObject):
     OBJECT_TYPE = "vm"
     __NETWORK_ADDRESSES = "networks"
-    __CPU_USAGE = "VCPUs-utilisation"   
+    __CPU_USAGE = "VCPUs-utilisation"
 
     def VBD(self):
         return self.getObjectsReferencing(VBD.OBJECT_TYPE, self.OBJECT_TYPE)
-    
+
     def VDI(self):
         return [x.VDI() for x in self.VBD()]
 
     def networkAddresses(self):
         return self.getListParam(self.__NETWORK_ADDRESSES)
-    
-    
+
+
     @property
     def cpuUsage(self):
         return self.getDictParam(self.__CPU_USAGE)
@@ -180,13 +180,13 @@ class VBD(XapiObject):
 
     def allowedOperations(self):
         return self.getListParam(self.__OPS)
-    
+
     def VDI(self):
         return self.getObjectParam(VDI.OBJECT_TYPE, self.__VDI_UUID)
 
 class XapiHost(NamedXapiObject):
     OBJECT_TYPE = "host"
-    
+
     def SR(self):
         return self.getObjectsReferencingName(SR.OBJECT_TYPE, self.OBJECT_TYPE)
 
@@ -197,17 +197,17 @@ class SR(NamedXapiObject):
 
     def isLocal(self):
         return re.search(self.__LOCAL, self.name())
-    
+
     def srType(self):
         return self.getStringParam(self.__TYPE)
-    
+
     def VDI(self):
         return self.getObjectsReferencing(VDI.OBJECT_TYPE, self.OBJECT_TYPE)
-        
+
 class VDI(NamedXapiObject):
     OBJECT_TYPE = "vdi"
     __SR_UUID = "sr-uuid"
-    
+
     def SR(self):
         return self.getObjectParam(SR.OBJECT_TYPE, self.__SR_UUID)
 
@@ -216,7 +216,7 @@ class VDI(NamedXapiObject):
 
 """
 Setup global factory and accessor method
-"""    
+"""
 ObjectFactoryInstance = None
 
 def objectFactory():
@@ -227,7 +227,7 @@ def objectFactory():
     if not ObjectFactoryInstance:
         ObjectFactoryInstance = XapiObjectFactory()
     return ObjectFactoryInstance
-    
+
 """
 Register objects with factory - allows specialisations to be
 registered and hence returned by the factory class
@@ -237,5 +237,5 @@ objectFactory().registerObject(VM)
 objectFactory().registerObject(SR)
 objectFactory().registerObject(XapiHost)
 objectFactory().registerObject(VDI)
-    
-    
+
+

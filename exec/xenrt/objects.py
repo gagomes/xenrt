0d6e9d76a45c3697c219034d9095c399f7ea746d
John Dilley <john.dilley@citrix.com>
2015-02-05 17:01:02 +0000

    OOBE

--- exec/xenrt/objects.py
+++ exec/xenrt/objects.py
@@ -4197,7 +4197,7 @@ Loop While not oex3.Stdout.atEndOfStream"""%(applicationEventLogger,systemEventL
 
         self.xmlrpcWriteFile("c:\\unattend.xml", unattend)
 
-        self.xmlrpcExec("c:\\windows\\system32\\sysprep\\sysprep.exe /unattend:c:\\unattend.xml /generalize /quiet /quit")
+        self.xmlrpcExec("c:\\windows\\system32\\sysprep\\sysprep.exe /unattend:c:\\unattend.xml /oobe /generalize /quiet /quit")
 
 class RunOnLocation(GenericPlace):
     def __init__(self, address):

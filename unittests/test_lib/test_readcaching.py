5e29adb5b84837230913878729c7a7994b1958ee
Chris Harding <chris.harding@citrix.com>
2015-02-17 17:02:03 +0000

    Refactor unit tests

--- unittests/test_lib/test_readcaching.py
+++ unittests/test_lib/test_readcaching.py
@@ -1,7 +1,6 @@
-from xenrt.lib.xenserver.readcaching import LowLevelReadCacheController, ReadCachingController, XapiReadCacheController
+from xenrt.lib.xenserver.readcaching import ReadCachingController
 from testing import XenRTUnitTestCase
-from mock import Mock, patch
-
+from mock import Mock
 
 
 class TestReadCachingLowLevelController(XenRTUnitTestCase):
@@ -27,47 +26,12 @@ class TestReadCachingLowLevelController(XenRTUnitTestCase):
         data = [("1", "2", "1-2", """{"reqs_oustanding": 0, "read_caching": "false" }"""),
                 ("2", "3", "2-3", """{"reqs_oustanding": 0, "read_caching": "true" }""")]
         host = self.__createMockHost(data)
-        rc = LowLevelReadCacheController(host, "2-3")
+        rc = ReadCachingController(host, "2-3")
         self.assertTrue(rc.isEnabled())
 
     def testTapCtlWithExpectedOutputReadCacheDisabled(self):
         data = [("1", "2", "1-2", """{"reqs_oustanding": 0, "read_caching": "false" }"""),
                 ("2", "3", "2-3", """{"reqs_oustanding": 0, "read_caching": "true" }""")]
         host = self.__createMockHost(data)
-        rc = LowLevelReadCacheController(host, "1-2")
+        rc = ReadCachingController(host, "1-2")
         self.assertFalse(rc.isEnabled())
-
-
-class TestReadCachingController(XenRTUnitTestCase):
-
-    @patch("xenrt.lib.xenserver.readcaching.XapiReadCacheController.isEnabled")
-    @patch("xenrt.lib.xenserver.readcaching.LowLevelReadCacheController.isEnabled")
-    def testLowLevelIsEnabledRedirect(self, llc, xc):
-        rcc = ReadCachingController(None, None)
-        rcc.isEnabled()
-        self.assertFalse(llc.called)
-        self.assertTrue(xc.called)
-
-    @patch("xenrt.lib.xenserver.readcaching.XapiReadCacheController.isEnabled")
-    @patch("xenrt.lib.xenserver.readcaching.LowLevelReadCacheController.isEnabled")
-    def testXapiIsEnabledRedirect(self, llc, xc):
-        rcc = ReadCachingController(None, None)
-        rcc.isEnabled(LowLevel=True)
-        self.assertTrue(llc.called)
-        self.assertFalse(xc.called)
-
-    @patch("xenrt.lib.xenserver.readcaching.XapiReadCacheController.disable")
-    @patch("xenrt.lib.xenserver.readcaching.LowLevelReadCacheController.disable")
-    def testXapiIsNotCalledForEnable(self, llc, xc):
-        rcc = ReadCachingController(None, None)
-        rcc.disable()
-        self.assertTrue(llc.called)
-        self.assertFalse(xc.called)
-
-    @patch("xenrt.lib.xenserver.readcaching.XapiReadCacheController.enable")
-    @patch("xenrt.lib.xenserver.readcaching.LowLevelReadCacheController.enable")
-    def testXapiIsNotCalledForDisable(self, llc, xc):
-        rcc = ReadCachingController(None, None)
-        rcc.enable()
-        self.assertTrue(llc.called)
-        self.assertFalse(xc.called)

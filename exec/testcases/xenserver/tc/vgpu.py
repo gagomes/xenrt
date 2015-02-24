bc29482516f52180a78867bb2b11dccc27afb751
Ronan Murphy <ronan.murphy2@citrix.com>
2015-02-24 18:00:46 +0000

    Using built in host.migrateVDI() function.

--- exec/testcases/xenserver/tc/vgpu.py
+++ exec/testcases/xenserver/tc/vgpu.py
@@ -2714,7 +2714,7 @@ class TCOpsonK2vGPUToVMhasGotvGPU(TCBasicVerifOfAllK2config):
             vbd = host.minimalList("vbd-list", args="vm-uuid=%s type=Disk" % g.getUUID())        
             vdi = host.minimalList("vdi-list", args="vbd-uuids=%s " % vbd[0])
             dest_sr=host.getSRs(type="nfs")[0]
-            host.getCLIInstance().execute("vdi-pool-migrate uuid=%s sr-uuid=%s" % (vdi[0], dest_sr))            
+            host.migrateVDI(vdi[0], dest_sr)
             raise xenrt.XRTFailure("VM's live vdi migration is successful on a vGPU capable VM")
         except:
             pass

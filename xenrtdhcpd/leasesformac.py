e5ed5bf5b8f954e9f1523b09b9cf244d7e8a6a4f
John Dilley <john.dilley@citrix.com>
2015-02-25 12:34:02 +0000

    Make sure the lease is at least 10 seconds old

--- xenrtdhcpd/leasesformac.py
+++ xenrtdhcpd/leasesformac.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-import psycopg2, json, sys
+import psycopg2, json, sys, time
 
 conn = psycopg2.connect("host='127.0.0.1' dbname='dhcp' user='dhcp' password='dhcp'")
 cur = conn.cursor()
 
-cur.execute("SELECT addr FROM leases WHERE mac=%s ORDER BY expiry DESC", [sys.argv[1]])
+cur.execute("SELECT addr FROM leases WHERE mac=%s AND leasestart<%s ORDER BY expiry DESC", [sys.argv[1], int(time.time()) - 10])
 
 print json.dumps([x[0] for x in cur.fetchall()])

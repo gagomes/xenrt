a9432f2fe4c77d3466bebe419ac24537d9dd0eb2
Vinod Pademar <vinod.pademar@citrix.com>
2015-04-13 18:00:15 +0100

    Using xenrt sleep

--- exec/testcases/xenserver/tc/multipath.py
+++ exec/testcases/xenserver/tc/multipath.py
@@ -3813,10 +3813,10 @@ class TC15464(xenrt.TestCase):
     def testMultipathSanity(self):
         self.disableRandomFCPorts()
         self.waitUntilDevicesAreInaccessible()
-        time.sleep(3) # handling CA-124429
+        xenrt.sleep(120) # handling CA-124429
         self.checkWhetherMultipathTopologyHasChanged()
         self.enableFCPorts()
-        time.sleep(120) # wait till devices are accessible
+        xenrt.sleep(120) # wait till devices are accessible
         self.checkSanityOfDevs()
         self.dev_list = self.getDevList() # the devices in the multipath group might change
 

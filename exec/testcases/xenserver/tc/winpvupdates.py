83dd2cad951c74dd2ce6e60b142fd6dcb6d063d4
Chethan Krishnappa <chetan.krishnappa@citrix.com>
2015-04-27 01:37:06 +0530

    Rearranging postrun

--- exec/testcases/xenserver/tc/winpvupdates.py
+++ exec/testcases/xenserver/tc/winpvupdates.py
@@ -18,7 +18,14 @@ class WindowsUpdateBase(xenrt.TestCase):
         self.snapshot = self.guest.snapshot()
         if self.args.has_key('TOOLS'):
             self.Tools = self.args['TOOLS']
-            
+
+    def postRun(self):
+        
+        log("Revert the VM back to the original state")
+        self.guest.revert(self.snapshot)
+        self.guest.lifecycleOperation("vm-start")
+        self.guest.removeSnapshot(self.snapshot)
+        
 class TCSnapRevertTools(WindowsUpdateBase):
     
     def run(self, arglist=None):
@@ -34,18 +41,13 @@ class TCSnapRevertTools(WindowsUpdateBase):
         
         log("Revert the VM to the State before tools were uninstalled")
         self.guest.revert(snapshot)
+        self.guest.removeSnapshot(snapshot)
         
         self.guest.start()
         
         log("Verify tools are present after the VM is reverted to previous state")
         if self.guest.checkPVDevicesState() and not self.guest.checkPVDriversStatus(ignoreException = True):
             raise xenrt.XRTFailure("PV Tools not present after the Reverting VM to state where tools were installed ")
-    
-    def postrun(self):
-
-        log("Revert the VM back to the original state")
-        self.guest.revert(self.snapshot)
-        self.guest.lifecycleOperation("vm-start")
         
 class TCSnapRevertNoTools(WindowsUpdateBase):
     
@@ -59,16 +61,101 @@ class TCSnapRevertNoTools(WindowsUpdateBase):
         
         log("Revert the VM to the State before tools were uninstalled")
         self.guest.revert(snapshot)
-        
+        self.guest.removeSnapshot(snapshot)
         self.guest.lifecycleOperation("vm-start")
         
         log("Verify tools are not present after the VM is reverted to previous state")
         if not self.guest.checkPVDevicesState():
             raise xenrt.XRTFailure("PV Tools present after the Reverting VM to state where tools were not installed ")
-    
-    def postrun(self):
 
-        log("Revert the VM back to the original state")
-        self.guest.revert(self.snapshot)
-        self.guest.lifecycleOperation("vm-start")
+class TCUpgWinCompatible(WindowsUpdateBase):
+    
+    def run(self, arglist=None):
+
+        log("Install Windows update compatible PV Drivers")
+        super(DundeeGuest , self).installDrivers(source = self.Tools)
         
+        log("Enable the Windows Updates from the Host")
+        self.guest.enableWindowsPVUpdates()
+        
+        log("Update PV Drivers on the windows guest")
+        self.guest.installDrivers()
+
+class TCUpgNonWinCmp(WindowsUpdateBase):
+
+    def run(self, arglist=None):
+
+        log("Install Non-Windows update compatible PV Drivers")
+        super(DundeeGuest , self).installDrivers(source = self.Tools)
+        
+        log("Uninstall Non-Windows Update Compatible PV Drivers")
+        super(DundeeGuest , self).uninstallDrivers()
+        
+        log("Enable the Windows Updates from the Host")
+        self.guest.enableWindowsPVUpdates()
+        
+        log("Install PV Drivers on the windows guest")
+        self.guest.installDrivers()
+
+class TCUpgToolsIso(WindowsUpdateBase):
+
+    def run(self, arglist=None):
+        
+        log("Install the PV Drivers on the Windows Guest")
+        self.guest.installDrivers(source = self.Tools)
+
+        log("Upgrade the tools using tools.iso")
+        super(DundeeGuest , self).installDrivers(waitForDaemon)
+
+class TCSkipPvPkg(WindowsUpdateBase):
+
+    def skipPvPkginst(self, pkgList):
+        
+        log("Take snapshot of the VM")
+        snapshot = self.snapshot()
+        
+        log("Install PV Drivers on the windows guest")
+        self.installDrivers(pkgList)
+        
+        self.waitForDaemon(300, desc="Guest check after installation of PV Packages %s" %(pkgList))
+        
+        log("Revert the VM to the State before tools were uninstalled")
+        self.revert(snapshot)
+        self.removeSnapshot(snapshot)
+        self.lifecycleOperation("vm-start")
+        
+    def run(self, arglist=None):
+
+        self.pvDriversList = xenrt.TEC().lookup("PV_DRIVERS_LIST").split(';')
+        
+        for pkg in self.pvDriversList:
+            self.guest.skipPvPkginst(random.sample(self.pvDriverList, 4))
+
+class TCRemovePvpkg(WindowsUpdateBase):
+
+
+    def removePvpkg(self,pvPkg):
+        
+        log("Take snapshot of the VM")
+        snapshot = self.snapshot()
+        
+        log("Install PV Drivers on the windows guest")
+        self.uninstallDrivers(pvPkg)
+        
+        self.waitForDaemon(300, desc="Guest check after Uninstallation of PV Packages %s" %(pkgList))
+        
+        log("Revert the VM to the State before tools were uninstalled")
+        self.revert(snapshot)
+        self.removeSnapshot(snapshot)
+        self.lifecycleOperation("vm-start")
+        
+    def run(self, arglist=None):
+        
+        self.pvDriversToRm = xenrt.TEC().lookup("PV_DRIVERS_UNINSTALL_LIST").split(';')
+        
+        log("Install PV Drivers on the windows guest")
+        self.installDrivers()
+        
+        for item in self.pvDriversToRm:
+            
+            self.removePvPkg(self.pvDriversToRm)

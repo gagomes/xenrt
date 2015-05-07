3adfde32b5e4ab3e752a5a0c458fb92839c3faf9
ramakrishp <ramakrishna.padyana@citrix.com>
2015-05-07 16:52:21 +0530

    trial and error to check

--- exec/testcases/xenserver/tc/bonding.py
+++ exec/testcases/xenserver/tc/bonding.py
@@ -1700,14 +1700,15 @@ class _FailoverBondTest(_AggregateBondTest):
                     self.check(expected_paths)
                     macs_disabled.remove(mac)
                 # Make sure we are not bringing all NIC's down : ( CA-149995 )
-                if len(macs_disabled) != total_paths - 1:
-                    self.disablePath(host, mac)
-                    time.sleep(100)
-                    expected_paths -= 1
-                    if expected_paths != 0:
-                        time.sleep(60)
-                        self.check(expected_paths)
-                    macs_disabled.add(mac)
+                else:
+                    if len(macs_disabled) != total_paths - 1:
+                        self.disablePath(host, mac)
+                        time.sleep(100)
+                        expected_paths -= 1
+                        if expected_paths != 0:
+                            time.sleep(60)
+                            self.check(expected_paths)
+                            macs_disabled.add(mac)
                     
         return
         

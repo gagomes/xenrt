ff6a1b2028012eb1caa72a4cded1ac4fe3a99164
Vivek Kumar Chaubey <vivekkumar.chaubey@citrix.com>
2015-06-05 16:46:15 +0530

    Revert "Improving try/except block"
    This reverts commit e1dcc3ae6172f832010f99d8bee8c5d53f88ec37.

--- exec/testcases/xenserver/tc/upgrade.py
+++ exec/testcases/xenserver/tc/upgrade.py
@@ -3411,12 +3411,9 @@ class ADUpgradeAuthentication(_FeatureOperationAfterUpgrade):
             xenrt.TEC().logverbose("Executing CLI command to add role by self")
             try:
                 cliRole.execute("subject-role-add", string.join(argsAddrole),\
-                    username="root",password=self.poolsToUpgrade[0].master.password)                                    
-            except xenrt.XRTException, e:
-                if "Role already exists" in e.reason:
-                    xenrt.TEC().logverbose("Role seems to be added already")
-                else:
-                    raise
+                    username="root",password=self.poolsToUpgrade[0].master.password)
+            except:
+                xenrt.TEC().logverbose("Role seems to be added already")
             xenrt.TEC().logverbose("adding role to the queue ")
             self.authserver.getSubject(name=user).roles.add("pool-admin")
 

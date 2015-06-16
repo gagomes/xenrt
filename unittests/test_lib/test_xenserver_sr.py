9ad1057ac1a61b9ccb6ce43ba5c50cf0593be163
JunWook Kwak <junwook.kwak@citrix.com>
2015-06-16 11:09:28 +0100

    Created new sr lib unittests.
    Moved sr lib unittests into sr unittest.

---
+++ unittests/test_lib/test_xenserver_sr.py
@@ -0,0 +1,37 @@
+from testing import XenRTUnitTestCase
+from mock import Mock
+import xenrt
+
+
+class TestNFSStorageRepository(XenRTUnitTestCase):
+    def testCreateStoresDeviceConfiguration(self):
+        host = Mock()
+        sr = xenrt.lib.xenserver.NFSStorageRepository(host, 'sr-name')
+
+        sr.create('guest-IP', '/nfs-export')
+
+        self.assertEquals(
+            {
+                'server': 'guest-IP',
+                'serverpath': '/nfs-export'
+            },
+            sr.dconf
+        )
+
+
+class TestNFSv4StorageRepository(XenRTUnitTestCase):
+    def testCreateUsesVersion4AsAParameter(self):
+        host = Mock()
+        sr = xenrt.lib.xenserver.NFSv4StorageRepository(host, 'sr-name')
+
+        sr.create('guest-IP', '/nfs-export')
+
+        self.assertEquals(
+            {
+                'server': 'guest-IP',
+                'serverpath': '/nfs-export',
+                'nfsversion': '4'
+            },
+            sr.dconf
+        )
+

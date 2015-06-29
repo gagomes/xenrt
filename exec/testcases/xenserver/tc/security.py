b4b0998514901e2c6acacad2f8f25a5c53fa886c
Craig Orendi <craig.orendi@citrix.com>
2015-06-29 13:36:14 +0100

    in __runAllPackageTests for IPv6Firewall change p to attacker.hCFirewall6Package

--- exec/testcases/xenserver/tc/security.py
+++ exec/testcases/xenserver/tc/security.py
@@ -3457,7 +3457,7 @@ class TempIPv6Firewall6(xenrt.TestCase, object):
         #------------------------------------------------------------
         step("Running attacks from %s on vm %s" % (attacker.getName(), victim.getName()))
         #------------------------------------------------------------
-        for tc in p.testCasesIds():
+        for tc in attacker.hCFirewall6Package.testCasesIds():
             self.runSubcase("_runPackageTestCase", (attacker.hCFirewall6Package, attacker, victim, tc), "HackersChoice Firewall6 on %s" % victim.getName(), "test %d" % tc)
 
     def run(self,arglist):

5ce6daa95fd83c6c65b45bec896fb751fbb04e4a
Craig Orendi <craig.orendi@citrix.com>
2015-06-29 13:21:20 +0100

    changed for loop in ipv6firewall run from v to victim and updated all parameters

--- exec/testcases/xenserver/tc/security.py
+++ exec/testcases/xenserver/tc/security.py
@@ -3474,8 +3474,8 @@ class TempIPv6Firewall6(xenrt.TestCase, object):
         #-------------------------
         attacker.installHCFirewall6Ubuntu(net.PRIVATE_NETWORK)
         victims = [Victim(t) for t in [xenrt.TEC().registry.guestGet(x) for x in attacker.getHost().listGuests()] if t.windows]
-        for v in victims:
-            ipv6Address = net.getIPV6AddressOfVM(v)
+        for victim in victims:
+            ipv6Address = net.getIPV6AddressOfVM(victim)
             self.__runAllPackageTests(attacker,victim,ipv6Address)
 
 class TCHackersChoiceIPv6Firewall6(TCBadPackets):

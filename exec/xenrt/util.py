88567291375dfc9efd2a76003899b603f547ecf9
John Dilley <john.dilley@citrix.com>
2015-07-09 09:00:21 +0000

    Fix getLinuxRepo

--- exec/xenrt/util.py
+++ exec/xenrt/util.py
@@ -1588,7 +1588,11 @@ def getUpdateDistro(distro):
     return newdistro
 
 def getLinuxRepo(distro, arch, method, default=xenrt.XRTError):
-
+    if isWindows(distro):
+        if default == xenrt.XRTError:
+            raise xenrt.XRTError("No repo for windows")
+        else:
+            return default
     if not arch:
         arch = "x86-32"
     if xenrt.TEC().lookup("LOOP_MOUNT_LINUX", False, boolean=True):

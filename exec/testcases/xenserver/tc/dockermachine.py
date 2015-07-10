cab49131d68282c63c2aa3ed7815f542f3ae817e
Phus Lu <phuslu@hotmail.com>
2015-07-10 15:05:43 +0800

    CP-12151: Add docker-machine cases

--- exec/testcases/xenserver/tc/dockermachine.py
+++ exec/testcases/xenserver/tc/dockermachine.py
@@ -7,27 +7,57 @@
 # conditions as licensed by Citrix Systems, Inc. All other rights reserved.
 
 import xenrt, xenrt.lib.xenserver
-from xenrt.lib.xenserver.docker import *
 
-class TCDockerMachineLifeCycle(TCDockerBase):
+import os
+import time
+
+class TCDockerMachineLifeCycle(xenrt.TestCase):
     """Docker Machine lifecycle tests"""
 
-    # Phus Please modify below 
-    NO_OF_CONTAINERS = 5
+    DOCKER_MACHINE_BINARY = 'https://marvin.do.citrite.net/job/docker-machine-gitlab/lastSuccessfulBuild/artifact/docker-machine_linux-amd64'
+    COMMAND = os.path.basename(DOCKER_MACHINE_BINARY)
 
-    def createDockerContainers(self):
+    def prepare(self, arglist=None):
+        # Obtain the pool object to retrieve its hosts.
+        self.pool = self.getDefaultPool()
+        if self.pool is None:
+            self.host = self.getDefaultHost()
+        else:
+            self.host = self.pool.master
+        self.vmname = 'test-%s' % time.strftime('%Y%m%d%H%M%S')
 
-        # Create some containers (say 5) of your choice in every guest.
-        [docker.createContainer(ContainerType.SLEEP_BUSYBOX) for cnum in range(self.NO_OF_CONTAINERS) for docker in self.docker]
+    def downloadDockerMachineBinary(self):
+        xenrt.TEC().logverbose("Download %r" % self.DOCKER_MACHINE_BINARY)
+        cmd = 'curl -O "%s" 2>&1' % self.DOCKER_MACHINE_BINARY
+        data = os.popen(cmd).read()
+        xenrt.TEC().logverbose(data)
+        if not os.path.exists(self.COMMAND):
+            raise xenrt.XRTFailure("Download %r error" % self.COMMAND)
+        os.system('/bin/rm -rf ~/.docker')
+        os.system('chmod +x ./%s' % self.COMMAND)
+        xenrt.TEC().logverbose("Download %r OK" % self.DOCKER_MACHINE_BINARY)
 
-    def lifeCycleDockerContainers(self):
+    def lifeCycleCreate(self):
+        host_ip = self.host.getIP()
+        boot2docker_url = 'https://github.com/tianon/boot2docker/releases/download/v1.7.1-rc2/boot2docker.iso'
+        cmd = './%s create --driver xenserver --xenserver-boot2docker-url "%s" --xenserver-server %s --xenserver-username root --xenserver-password xenroot %s 2>&1' % (self.COMMAND, boot2docker_url, host_ip, self.vmname)
+        data = os.popen(cmd).read()
+        xenrt.TEC().logverbose(data)
+        if self.vmname not in self.host.listGuests(running=True):
+            raise xenrt.XRTFailure("VM(%r) not running at XenServer %s" % (self.vmname, host_ip))
 
-        # Lifecycle tests on all containers in every guest.
-        [docker.lifeCycleAllContainers() for docker in self.docker]
+    def lifeCycleRestart(self):
+        cmd = './%s restart %s 2>&1' % (self.COMMAND, self.vmname)
+        data = os.popen(cmd).read()
+        xenrt.TEC().logverbose(data)
+        if self.vmname not in self.host.listGuests(running=True):
+            raise xenrt.XRTFailure("VM(%r) not running at XenServer %s" % (self.vmname, host_ip))
+
+    def lifeCycleDockerMachine(self):
+        self.lifeCycleCreate()
+        self.lifeCycleRestart()
 
     def run(self, arglist=None):
-        xenrt.TEC().logverbose("Create enough containers in every guests")
-        self.createDockerContainers()
-        xenrt.TEC().logverbose("Perform life cycle operations on all containers")
-        self.lifeCycleDockerContainers()
+        self.downloadDockerMachineBinary()
+        self.lifeCycleDockerMachine()
 

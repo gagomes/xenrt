dad2022b651fe8f8c6644e8b803b153a6111196d
Phus Lu <phus.lu@citrix.com>
2015-07-13 19:42:18 +0800

    CP-12151: Refine code as Dan's comments

--- exec/testcases/xenserver/tc/dockermachine.py
+++ exec/testcases/xenserver/tc/dockermachine.py
@@ -14,8 +14,8 @@ import time
 class TCDockerMachineLifeCycle(xenrt.TestCase):
     """Docker Machine lifecycle tests"""
 
-    DOCKER_MACHINE_BINARY = 'https://marvin.do.citrite.net/job/docker-machine-gitlab/lastSuccessfulBuild/artifact/docker-machine_linux-amd64'
-    COMMAND = os.path.basename(DOCKER_MACHINE_BINARY)
+    DOCKER_MACHINE_URL = 'https://marvin.do.citrite.net/job/docker-machine-gitlab/lastSuccessfulBuild/artifact/docker-machine_linux-amd64'
+    BOOT2DOCKER_URL = 'https://github.com/tianon/boot2docker/releases/download/v1.7.1-rc2/boot2docker.iso'
 
     def prepare(self, arglist=None):
         # Obtain the pool object to retrieve its hosts.
@@ -24,32 +24,31 @@ class TCDockerMachineLifeCycle(xenrt.TestCase):
             self.host = self.getDefaultHost()
         else:
             self.host = self.pool.master
-        self.vmname = 'test-%s' % time.strftime('%Y%m%d%H%M%S')
+        self.vmname = xenrt.randomGuestName()
+        self.command = ''
 
     def downloadDockerMachineBinary(self):
-        xenrt.TEC().logverbose("Download %r" % self.DOCKER_MACHINE_BINARY)
-        cmd = 'curl -O "%s" 2>&1' % self.DOCKER_MACHINE_BINARY
-        data = os.popen(cmd).read()
-        xenrt.TEC().logverbose(data)
-        if not os.path.exists(self.COMMAND):
-            raise xenrt.XRTFailure("Download %r error" % self.COMMAND)
-        os.system('/bin/rm -rf ~/.docker')
-        os.system('chmod +x ./%s' % self.COMMAND)
-        xenrt.TEC().logverbose("Download %r OK" % self.DOCKER_MACHINE_BINARY)
+        xenrt.TEC().logverbose("Download %r" % self.DOCKER_MACHINE_URL)
+        self.command = xenrt.TEC().getFile(self.DOCKER_MACHINE_URL)
+        try:
+            xenrt.checkFileExists(self.command)
+        except:
+            raise xenrt.XRTFailure("Download %r to %r error" % (self.DOCKER_MACHINE_URL, self.command))
+        cmd = 'chmod +x %s' % self.command
+        if 0 != xenrt.command(cmd, retval="code"):
+            raise xenrt.XRTFailure("Failed to %r" % cmd)
+        xenrt.TEC().logverbose("Download %r OK" % self.DOCKER_MACHINE_URL)
 
     def lifeCycleCreate(self):
         host_ip = self.host.getIP()
-        boot2docker_url = 'https://github.com/tianon/boot2docker/releases/download/v1.7.1-rc2/boot2docker.iso'
-        cmd = './%s create --driver xenserver --xenserver-boot2docker-url "%s" --xenserver-server %s --xenserver-username root --xenserver-password xenroot %s 2>&1' % (self.COMMAND, boot2docker_url, host_ip, self.vmname)
-        data = os.popen(cmd).read()
-        xenrt.TEC().logverbose(data)
+        cmd = '%s create --driver xenserver --xenserver-boot2docker-url "%s" --xenserver-server %s --xenserver-username root --xenserver-password xenroot %s 2>&1' % (self.command, self.BOOT2DOCKER_URL, host_ip, self.vmname)
+        xenrt.command(cmd, timeout=1800)
         if self.vmname not in self.host.listGuests(running=True):
             raise xenrt.XRTFailure("VM(%r) not running at XenServer %s" % (self.vmname, host_ip))
 
     def lifeCycleRestart(self):
-        cmd = './%s restart %s 2>&1' % (self.COMMAND, self.vmname)
-        data = os.popen(cmd).read()
-        xenrt.TEC().logverbose(data)
+        cmd = '%s restart %s 2>&1' % (self.command, self.vmname)
+        xenrt.command(cmd, timeout=600)
         if self.vmname not in self.host.listGuests(running=True):
             raise xenrt.XRTFailure("VM(%r) not running at XenServer %s" % (self.vmname, host_ip))
 

a158e49d209bd7ee08dcfdd66367684c4a4dcc14
John Dilley <john.dilley@citrix.com>
2015-08-05 09:50:44 +0000

    Add docstring

--- exec/testcases/xenserver/tc/melio.py
+++ exec/testcases/xenserver/tc/melio.py
@@ -1,6 +1,7 @@
 import xenrt
 
 class TC27207(xenrt.TestCase):
+    """Very basic smoke test of MelioFS on XenServer"""
     def run(self, arglist):
         h = self.getDefaultHost()
         h.installMelio()

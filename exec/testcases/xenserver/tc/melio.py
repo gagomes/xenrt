ae09abc7b14b0f580eabb8258d690411599f2324
John Dilley <john.dilley@citrix.com>
2015-08-05 09:03:43 +0000

    Rename

--- exec/testcases/xenserver/tc/melio.py
+++ exec/testcases/xenserver/tc/melio.py
@@ -4,8 +4,8 @@ class TC27207(xenrt.TestCase):
     def run(self, arglist):
         h = self.getDefaultHost()
         h.installMelio()
-        h.melio.setup()
-        h.melio.mount("/mnt")
+        h.melioHelper.setup()
+        h.melioHelper.mount("/mnt")
         h.execdom0("echo 'Testing' > /mnt/testing")
         if h.execdom0("cat /mnt/testing").strip() != "Testing":
             raise xenrt.XRTFailure("File read did not match write")

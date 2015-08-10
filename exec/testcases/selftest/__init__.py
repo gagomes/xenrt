a2701ca6a9502417948ff7fae68c48a19a680983
Alex Brett <alex.brett@citrix.com>
2015-08-10 13:45:33 +0100

    Fix PDU fallback test

--- exec/testcases/selftest/__init__.py
+++ exec/testcases/selftest/__init__.py
@@ -186,7 +186,7 @@ class TCMachineCheck(xenrt.TestCase):
         """Verify the host is on the correct PDU port (either as primary or fallback)"""
         powerctl = self.host.machine.powerctl
         if isinstance(powerctl, xenrt.powerctl.IPMIWithPDUFallback):
-            powerctl = powerctl.pdu
+            powerctl = powerctl.PDU
         
         if not isinstance(powerctl, xenrt.powerctl.PDU):
             raise xenrt.XRTSkip("No PDU support found")

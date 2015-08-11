279e74c435a420fc7ead1fb927c5c2fd0da2de20
Sreedhar Reddy <sreedhar.reddy@citrix.com>
2015-08-11 18:07:07 +0530

    reverted previous changes for rhel guests

--- exec/testcases/xenserver/tc/upgrade.py
+++ exec/testcases/xenserver/tc/upgrade.py
@@ -2341,7 +2341,7 @@ class TC12548(_TCCrossVersionImport):
 class TC12545(_TCCrossVersionImport):
     """Import of VMs exported from XenServer 5.6 GA (MNR)"""
 
-    DISTROS = ["rhel5x", "rhel4x"]
+    DISTROS = ["rhel53", "rhel47"]
 
 class TC17440(_TCCrossVersionImport):
     """Import of VMs exported from XenServer 6.0 GA (Boston)"""

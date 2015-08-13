6987132dbf88d1483aa18f0f417f7aa54b009ec8
John Dilley <john.dilley@citrix.com>
2015-08-13 11:12:45 +0000

    Support multiple melio hosts

--- exec/xenrt/lib/xenserver/melio.py
+++ exec/xenrt/lib/xenserver/melio.py
@@ -18,16 +18,21 @@ __all__ = [
            
 
 class MelioHelper(object):
-    def __init__(self, host):
-        self.host = host
+    def __init__(self, hosts, iscsiHost=None):
+        self.hosts = hosts
         self.lun = None
+        self._iscsiHost = iscsiHost
+
+    @property
+    def iscsiHost(self):
+        return self._iscsiHost or self.hosts[0]
 
     def setup(self):
         self.installMelio()
         self.setupISCSITarget()
         self.setupMelioDisk()
 
-    def getRpmToDom0(self, var, specVar, dest):
+    def getRpmToDom0(self, host, var, specVar, dest):
         rpm = xenrt.TEC().lookup(var, None)
         if not rpm:
             return False
@@ -40,65 +45,70 @@ class MelioHelper(object):
             return False
         d = xenrt.WebDirectory()
         d.copyIn(f)
-        self.host.execdom0("wget -O %s %s" % (dest, d.getURL(os.path.basename(f))))
+        host.execdom0("wget -O %s %s" % (dest, d.getURL(os.path.basename(f))))
         return True
 
     def installMelio(self, reinstall=False):
-            
-        if self.host.execdom0("lsmod | grep warm_drive", retval="code") == 0 and not reinstall:
+        tasks = [xenrt.PTask(self.installMelioOnHost, x, reinstall) for x in self.hosts]
+        xenrt.pfarm(tasks)
+    
+    def installMelioOnHost(self, host, reinstall=False):
+        if host.execdom0("lsmod | grep warm_drive", retval="code") == 0 and not reinstall:
             return
         d = xenrt.WebDirectory()
         if xenrt.TEC().lookup("PATCH_SMAPI_RPMS", False, boolean=True):
-            self.host.execdom0("mkdir -p /root/smapi_rpms")
+            host.execdom0("mkdir -p /root/smapi_rpms")
             rpms = ['xapi-core-*.rpm', 'xapi-storage-script-*.rpm', 'xenopsd-0*.rpm', 'xenopsd-xc-*.rpm', 'xenopsd-xenlight-*.rpm', 'xapi-storage-0*.rpm']
             for r in rpms:
                 f = xenrt.TEC().getFile("/usr/groups/xen/carbon/trunk-btrfs-3/latest/binary-packages/RPMS/domain0/RPMS/x86_64/%s" % r)
                 d.copyIn(f)
-                self.host.execdom0("wget -O /root/smapi_rpms/%s %s" % (os.path.basename(f), d.getURL(os.path.basename(f))))
-            self.host.execdom0("rpm -Uv --force /root/smapi_rpms/*.rpm")
-            self.host.resetToFreshInstall(setupISOs=True)
-        self.host.execdom0("yum install -y boost boost-atomic boost-thread boost-filesystem")
-        if not self.getRpmToDom0("MELIO_RPM", "melio_rpm", "/root/melio.rpm"):
+                host.execdom0("wget -O /root/smapi_rpms/%s %s" % (os.path.basename(f), d.getURL(os.path.basename(f))))
+            host.execdom0("rpm -Uv --force /root/smapi_rpms/*.rpm")
+            host.resetToFreshInstall(setupISOs=True)
+        host.execdom0("yum install -y boost boost-atomic boost-thread boost-filesystem")
+        if not self.getRpmToDom0(host, "MELIO_RPM", "melio_rpm", "/root/melio.rpm"):
             raise xenrt.XRTError("MELIO_RPM not found")
-        self.host.execdom0("rpm -U --replacepkgs /root/melio.rpm")
+        host.execdom0("rpm -U --replacepkgs /root/melio.rpm")
 
         # Workaround for now - load the melio stuff after boot
 
-        self.host.execdom0("sed -i /warm_drive/d /etc/rc.d/rc.local")
-        self.host.execdom0("sed -i /warm-drive/d /etc/rc.d/rc.local")
-        self.host.execdom0("echo 'modprobe warm_drive' >> /etc/rc.d/rc.local")
-        self.host.execdom0("chkconfig warm-drived off")
-        self.host.execdom0("echo 'service warm-drived start' >> /etc/rc.d/rc.local")
-        if self.host.execdom0("test -e /lib/systemd/system/warm-drive-webserverd.service", retval="code") == 0:
-            self.host.execdom0("chkconfig warm-drive-webserverd off")
-            self.host.execdom0("echo 'service warm-drive-webserverd start' >> /etc/rc.d/rc.local")
-        self.host.reboot()
-        self.checkXapiResponsive()
-        if self.getRpmToDom0("FFS_RPM", "ffs_rpm", "/root/ffs.rpm"):
+        host.execdom0("sed -i /warm_drive/d /etc/rc.d/rc.local")
+        host.execdom0("sed -i /warm-drive/d /etc/rc.d/rc.local")
+        host.execdom0("echo 'modprobe warm_drive' >> /etc/rc.d/rc.local")
+        host.execdom0("chkconfig warm-drived off")
+        host.execdom0("echo 'service warm-drived start' >> /etc/rc.d/rc.local")
+        if host.execdom0("test -e /lib/systemd/system/warm-drive-webserverd.service", retval="code") == 0:
+            host.execdom0("chkconfig warm-drive-webserverd off")
+            host.execdom0("echo 'service warm-drive-webserverd start' >> /etc/rc.d/rc.local")
+        host.reboot()
+        self.checkXapiResponsive(host)
+        if self.getRpmToDom0(host, "FFS_RPM", "ffs_rpm", "/root/ffs.rpm"):
             # RPM workaround for now
-            self.host.execdom0("rm -rf /usr/libexec/xapi-storage-script/datapath/raw+file*")
-            self.host.execdom0("rpm -U --replacepkgs /root/ffs.rpm")
+            host.execdom0("rm -rf /usr/libexec/xapi-storage-script/datapath/raw+file*")
+            host.execdom0("rpm -U --replacepkgs /root/ffs.rpm")
 
-    def checkXapiResponsive(self):
+    def checkXapiResponsive(self, host):
         for i in xrange(20):
             start = xenrt.timenow()
-            self.host.getCLIInstance().execute("vm-list")
+            host.getCLIInstance().execute("vm-list")
             if xenrt.timenow() - start > 10:
                 raise xenrt.XRTError("vm-list took > 10 seconds after installing melio")
 
     def setupISCSITarget(self):
-        self.lun = xenrt.ISCSIVMLun(targetType="LIO", sizeMB=100*xenrt.KILO)
-        self.host.execdom0("iscsiadm -m discovery -t st -p %s" % self.lun.getServer())
-        self.host.execdom0('iscsiadm -m node --targetname "%s" --portal "%s:3260" --login' % (self.lun.getTargetName(), self.lun.getServer()))
+        self.lun = xenrt.ISCSIVMLun(targetType="LIO", sizeMB=100*xenrt.KILO, host=self.iscsiHost)
+        for host in self.hosts:
+            host.execdom0("iscsiadm -m discovery -t st -p %s" % self.lun.getServer())
+            host.execdom0('iscsiadm -m node --targetname "%s" --portal "%s:3260" --login' % (self.lun.getTargetName(), self.lun.getServer()))
 
     @property
     def device(self):
         return "/dev/disk/by-id/scsi-%s" % self.lun.getID()
 
     def setupMelioDisk(self):
-        self.host.execdom0("/usr/sbin/wd_format warm_fs mount_1234 %s" % self.device)
+        self.hosts[0].execdom0("/usr/sbin/wd_format warm_fs mount_1234 %s" % self.device)
 
     def mount(self, mountpoint):
-        self.host.execdom0("mount -t warm_fs %s %s" % (self.device, mountpoint))
+        for host in self.hosts:
+            host.execdom0("mount -t warm_fs %s %s" % (self.device, mountpoint))
     
 

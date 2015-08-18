ce48e4da2fbadaa620e358fc2cc42ec27cd24b66
John Dilley <john.dilley@citrix.com>
2015-08-18 10:41:47 +0000

    Add TCDDIntegrity

--- exec/testcases/xenserver/tc/melio.py
+++ exec/testcases/xenserver/tc/melio.py
@@ -21,3 +21,16 @@ class TCSparseWrite(xenrt.TestCase):
         for i in range(20):
             xenrt.sleep(30)
             host.execdom0("python %s/remote/sparsewrite.py /mnt 1000" % xenrt.TEC().lookup("REMOTE_SCRIPTDIR"))
+
+class TCDDIntegrity(xenrt.TestCase):
+    def run(self, arglist):
+        host = self.getHost("RESOURCE_HOST_0")
+        host.melioHelper.checkMount("/mnt")
+        for i in range(20):
+            host.execdom0("rm -f /mnt/testdd*")
+            host.execdom0("dd if=/dev/urandom of=/mnt/testdd1 oflag=direct count=100 bs=1M")
+            host.execdom0("dd if=/mnt/testdd1 of=/mnt/testdd2 iflag=direct oflag=direct bs=1M")
+            md5sum1 = host.execdom0("md5sum /mnt/testdd1").strip()
+            md5sum2 = host.execdom0("md5sum /mnt/testdd2").strip()i
+            if md5sum1 != md5sum2:
+                raise xenrt.XRTFailure("md5sums did not match")

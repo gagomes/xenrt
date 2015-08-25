f8c5f72005365f1877ec6f7b9f964723dde28fbb
Akash Chandrashekar <akash.chandrashekar@citrix.com>
2015-08-25 14:32:48 +0530

    CA-180026: Licensing the host before creating CIFS SR.

--- exec/testcases/xenserver/tc/storage.py
+++ exec/testcases/xenserver/tc/storage.py
@@ -5059,6 +5059,7 @@ class TC26974(xenrt.TestCase):
     def run(self, arglist):
 
         self.host = self.host.upgrade()
+        self.host.license(edition="enterprise-per-socket")
         share = xenrt.VMSMBShare()
         sr = xenrt.productLib(host=self.host).SMBStorageRepository(self.host, "CIFS-SR")
         sr.create(share)

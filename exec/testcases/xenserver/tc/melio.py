f146164a8a2c6700806c89ba0e52b9953dff2bc3
John Dilley <john.dilley@citrix.com>
2015-08-28 15:45:01 +0000

    Adapt tests to run on cluster

--- exec/testcases/xenserver/tc/melio.py
+++ exec/testcases/xenserver/tc/melio.py
@@ -25,9 +25,11 @@ class TCMelioSmoke(TCMelioSetup):
     def run(self, arglist):
         melioHelper = self.setupMelio(sharedISCSI=True)
         melioHelper.mount("/mnt")
-        host = self.getHost("RESOURCE_HOST_0")
-        host.execdom0("echo 'Testing' > /mnt/testing")
-        if host.execdom0("cat /mnt/testing").strip() != "Testing":
+        h1 = self.getHost("RESOURCE_HOST_0")
+        h2 = h1.melioHelper.hosts[-1]
+
+        h1.execdom0("echo 'Testing' > /mnt/testing")
+        if h2.execdom0("cat /mnt/testing").strip() != "Testing":
             raise xenrt.XRTFailure("File read did not match write")
 
 class TCSparseWrite(xenrt.TestCase):
@@ -41,14 +43,15 @@ class TCSparseWrite(xenrt.TestCase):
 
 class TCDDIntegrity(xenrt.TestCase):
     def run(self, arglist):
-        host = self.getHost("RESOURCE_HOST_0")
-        host.melioHelper.checkMount("/mnt")
+        h1 = self.getHost("RESOURCE_HOST_0")
+        h2 = h1.melioHelper.hosts[-1]
+        h1.melioHelper.checkMount("/mnt")
         for i in range(20):
-            host.execdom0("rm -f /mnt/testdd*")
-            host.execdom0("dd if=/dev/urandom of=/mnt/testdd1 oflag=direct count=100 bs=1M")
-            host.execdom0("dd if=/mnt/testdd1 of=/mnt/testdd2 iflag=direct oflag=direct bs=1M")
-            md5sum1 = host.execdom0("md5sum /mnt/testdd1 | awk '{print $1}'").strip()
-            md5sum2 = host.execdom0("md5sum /mnt/testdd2 | awk '{print $1}'").strip()
+            h1.execdom0("rm -f /mnt/testdd*")
+            h1.execdom0("dd if=/dev/urandom of=/mnt/testdd1 oflag=direct count=100 bs=1M")
+            h2.execdom0("dd if=/mnt/testdd1 of=/mnt/testdd2 iflag=direct oflag=direct bs=1M")
+            md5sum1 = h2.execdom0("md5sum /mnt/testdd1 | awk '{print $1}'").strip()
+            md5sum2 = h1.execdom0("md5sum /mnt/testdd2 | awk '{print $1}'").strip()
             if md5sum1 != md5sum2:
                 raise xenrt.XRTFailure("md5sums did not match")
 

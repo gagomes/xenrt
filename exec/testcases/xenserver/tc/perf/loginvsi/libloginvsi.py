4785c848b9ba5bf1509caabbef24b9d28f238611
Vivek Kumar Chaubey <vivekkumar.chaubey@citrix.com>
2015-09-01 14:59:42 +0530

    adding loginvsi setup methods

--- exec/testcases/xenserver/tc/perf/loginvsi/libloginvsi.py
+++ exec/testcases/xenserver/tc/perf/loginvsi/libloginvsi.py
@@ -14,14 +14,18 @@ class LoginVSI(object):
         self.shareFolderPath =r"c:\%s" % self.shareFolderName
         self.shareFolderNetworkPath = r"\\%s\%s" % (self.dataServerGuest.getIP(), self.shareFolderName)
         self.vsishareDrive = "S"
-        self.vsisharePath = "%s:\\" % self.vsishareDrive
+        self.vsisharePath = "%s:" % self.vsishareDrive
 
         self.initdistfileVars()
 
     def initdistfileVars(self):
         self.config = { "distfileDrive"     : "Z",
                         "officeSetup"       : "officeSetup\off2k7\setup.exe",
-                        "officeSetupConfig" : "officeSetup\config.xml"  }
+                        "officeSetupConfig" : "officeSetup\config.xml",
+                        "dataserverZipFile" : "dataserver\vsishareFiles.zip",
+                        "7zipExe"           : "7-Zip\7z.exe",
+                        "targetSetup"       : "target\lib\VSITarget.cmd"
+                        }
 
         if self.version=="4.1.3":
             self.config.update({"distfileLocation" : r"\\%s\share\vol\xenrtdata\distfiles\performance\vsi413\xenrtFiles"% xenrt.TEC().lookup("XENRT_SERVER_ADDRESS")})
@@ -42,15 +46,21 @@ class LoginVSI(object):
         guest.xmlrpcExec(r"icacls %s /grant:r Everyone:(OI)(CI)F /T /C " % (self.shareFolderPath))
 
     def _installDataServer(self, guest):
-        # TODO
-        pass
+        guest.xmlrpcExec(r"%s x -o%s -y -bd %s" % (self.config["7zipExe"], self.shareFolderPath, self.config["dataserverZipFile"]))
+        # TODO - configure ini files.
 
     def _mapVSIShareToDrive(self, guest):
         guest.xmlrpcMapDrive(self.shareFolderNetworkPath, self.vsishareDrive)
 
+    def _createSubstPaths(self, guest):
+        guest.xmlrpcExec(r"subst h: /D", level=xenrt.RC_OK)
+        guest.xmlrpcExec(r"subst h: %Temp%")
+        guest.xmlrpcExec(r"subst g: /D", level=xenrt.RC_OK)
+        guest.xmlrpcExec(r"subst g: %s\_VSI_Content" %(vsi.vsisharePath))
+
     def _installTarget(self, guest):
-        # TODO
-        pass
+        guest.xmlrpcExec(r'setx vsishare "%s"' % (self.vsisharePath))
+        guest.xmlrpcExec(r'%s 1 1 1 1 1 "LoginVSI"' % self.config["targetSetup"])
 
     def setupDataServer(self):
         self._mapLoginVSIdistfiles(self.dataServerGuest)
@@ -64,6 +74,7 @@ class LoginVSI(object):
             self._installOffice(self.targetGuest)
         self._mapVSIShareToDrive(self.targetGuest)
         self._installTarget(self.targetGuest)
+        self._createSubstPaths(guest)
 
     def installLoginVSI(self):
         self.setupDataServer()

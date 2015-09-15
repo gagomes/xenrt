040944235faad1698f2697ad7bc84378eb5f96ea
Peenu Singh <peenu.singh@citrix.com>
2015-09-15 13:56:52 +0000

    Review Code changes

--- exec/testcases/xenserver/security.py
+++ exec/testcases/xenserver/security.py
@@ -558,7 +558,7 @@ class TCXSA112(_TCXSA):
         #change log level for pre dundee hosts
         if not isinstance(self.host, xenrt.lib.xenserver.DundeeHost):
             self.host.execdom0("sed -e 's/\(append .*xen\S*.gz\)/\\0 loglvl=all guest_loglvl=all/' /boot/extlinux.conf > tmp && mv tmp /boot/extlinux.conf -f")
-        self.host.reboot()
+            self.host.reboot()
         self.replaceHvmloader("/usr/groups/xenrt/xsa_test_files/test-hvm-xsa-112")
         
     def run(self, arglist=None):

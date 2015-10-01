19183765fa2710126b3ca145285f8b109bbc2d24
Craig Orendi <craig.orendi@citrix.com>
2015-10-01 11:30:56 +0100

    changes for testing

--- exec/testcases/xenserver/tc/dotnetagentlicensing.py
+++ exec/testcases/xenserver/tc/dotnetagentlicensing.py
@@ -90,17 +90,18 @@ class DotNetAgentTestCases(xenrt.TestCase):
                 raise xenrt.XRTFailure("Server was not pinged when it should be")     
 
     def postRun(self):
-        self.adapter.cleanupLicense(self.getDefaultPool())
+        #self.adapter.cleanupLicense(self.getDefaultPool())
         self.adapter.serverCleanup(self.getGuest("server"))
         self.adapter.settingsCleanup(self.getGuest("WS2012"))
 
 class TempTest(DotNetAgentTestCases):
 
     def run(self,arglist):
-        server = self.adapter.setUpServer(self.getGuest("server"),"16000")
-        self.adapter.applyLicense(self.getDefaultPool())
-        autoupdate = self.agent.getLicensedFeature("AutoUpdate")
-        self.getGuest("WS2012").enableVSS()
+        self.adapter.cleanupLicense(self.getDefaultPool())
+        #server = self.adapter.setUpServer(self.getGuest("server"),"16000")
+        self.adapter.applyLicense(self.getDefaultHost())
+        #autoupdate = self.agent.getLicensedFeature("AutoUpdate")
+        #self.getGuest("WS2012").enableVSS()
 
 class PoolAutoUpdateToggle(DotNetAgentTestCases):
 

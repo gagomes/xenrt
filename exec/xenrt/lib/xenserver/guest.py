685eb51a5f3dc74d0afdd4d48063b27c5a85a086
Shweta Tiwari <shweta.tiwari@citrix.com>
2015-10-09 16:24:54 +0530

    Fixing rootdisk for win10

--- exec/xenrt/lib/xenserver/guest.py
+++ exec/xenrt/lib/xenserver/guest.py
@@ -368,7 +368,7 @@ class Guest(xenrt.GenericGuest):
                                      "(arch %s)" % (distro, arch))
 
         self.isoname = isoname
-        if self.memory and self.isoname and ([i for i in ["win81","ws12r2"] if i in self.isoname]):
+        if self.memory and self.isoname and ([i for i in ["win81","ws12r2","win10"] if i in self.isoname]):
             if rootdisk == self.DEFAULT:
                 rootdisk = max(32768, 20480 + self.memory)
             else:

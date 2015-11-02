e6d6d266394655305d788353277c7b11b131c5f8
Alex Brett <alex.brett@citrix.com>
2015-11-02 15:53:42 +0000

    Fix call

--- exec/testcases/scalextreme/poc.py
+++ exec/testcases/scalextreme/poc.py
@@ -9,11 +9,11 @@ class SXDeployTest(xenrt.TestCase):
         # Find the Debian template
         template = self.getGuest("Debian Wheezy 7.0")
         host = template.getHost()
-        sxp = SXProcess("57994", "1", template.password, "2947")
+        sxp = SXProcess("57994", "1", "2947")
 
         # Find our providerId (TODO: we should store this in the registry)
         providerName = "xenrt-%d" % xenrt.GEC().jobid()
         providerId = [x['providerId'] for x in sxp.apiHandler.execute(category="providers") if x['providerName'] == providerName][0]
         
-        sxp.deploy(providerId, host, template.getUUID())
+        sxp.deploy(providerId, host, template.getUUID(), template.password)
 

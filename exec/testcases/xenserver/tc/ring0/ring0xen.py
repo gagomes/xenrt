c2e5de95e81ab5c08f7aaf822832b0a24148b1f9
Anshul Makkar <anshul.makkar@citrix.com>
2015-11-04 09:17:16 +0000

    CP-14639: Xen-test-framework: Incorporated the code review comments.

--- exec/testcases/xenserver/tc/ring0/ring0xen.py
+++ exec/testcases/xenserver/tc/ring0/ring0xen.py
@@ -12,15 +12,12 @@ class XenTestRun(object):
 #        xenrt.TEC().comment('Test %s Result: %s' % self.name, res)
         if res == "SUCCESS":
             xenrt.TEC().logverbose('Test %s Result: PASS' % self.name)
-            return 0
         else:
             raise xenrt.XRTFailure('Test %s Result: FAILED' % self.name)
-            return -1
 
     def run(self):
         self.host.execdom0("xl create /opt/xen-test-framework/%s.cfg" % self.name)
-        ret = self.results()
-        return ret
+        self.results()
 
 class TCRing0XenBase(xenrt.TestCase):
     def prepare(self, arglist):
@@ -28,44 +25,43 @@ class TCRing0XenBase(xenrt.TestCase):
 
         #install the xen-test-framework which is a prerequisite for running tests.
         step("install xen-test-framework RPM and copy other pre-requisites")
-        module_path = "/tmp/xen-test-framework.rpm"
-        module_rpm = xenrt.TEC().getFile("/usr/groups/build/trunk-ring0/latest/binary-packages/RPMS/domain0/RPMS/x86_64/xen-test-framework-*.rpm")
+        modulePath = "/tmp/xen-test-framework.rpm"
+        moduleRpm = xenrt.TEC().getFile("/usr/groups/build/trunk-ring0/latest/binary-packages/RPMS/domain0/RPMS/x86_64/xen-test-framework-*.rpm")
 
         try:
-            xenrt.checkFileExists(module_rpm)
-        except:
-            raise xenrt.XRTError("xen-test-framework rpm file is not present in the build \n")
+            xenrt.checkFileExists(moduleRpm)
+        except xenrt.XRTException, e:
+            raise xenrt.XRTError(e.reason)
 
         sh = self.host.sftpClient()
-
         try:
-            sh.copyTo(module_rpm, module_path)
+            sh.copyTo(moduleRpm, modulePath)
         finally:
             sh.close()
 
-        self.host.execdom0("rpm --force -Uvh %s" % (module_path))
+        self.host.execdom0("rpm --force -Uvh %s" % (modulePath))
         self.host.execdom0("xenconsoled --log=all")
 
 class TCRing0XenPV32Test(TCRing0XenBase):
     def run(self, arglist):
         step("32bit PV test")
-        xen_pv32_test = XenTestRun(self.host, "test-pv32-example");
-        xen_pv32_test.run()
+        xenPV32Test = XenTestRun(self.host, "test-pv32-example");
+        xenPV32Test.run()
 
 class TCRing0XenPV64Test(TCRing0XenBase):
     def run(self, arglist):
         step("64bit PV Test")
-        xen_pv64_test = XenTestRun(self.host, "test-pv64-example");
-        xen_pv64_test.run()
+        xenPV64Test = XenTestRun(self.host, "test-pv64-example");
+        xenPV64Test.run()
 
 class TCRing0XenHVM32Test(TCRing0XenBase):
     def run(self, arglist):
         step("32bit HVM test")
-        xen_hvm32_test = XenTestRun(self.host, "test-hvm32-example");
-        xen_hvm32_test.run()
+        xenHvm32Test = XenTestRun(self.host, "test-hvm32-example");
+        xenHvm32Test.run()
 
 class TCRing0XenHVM64Test(TCRing0XenBase):
     def run(self, arglist):
         step("64bit HVM test")
-        xen_hvm64_test = XenTestRun(self.host, "test-hvm-64-example");
-        xen_hvm64_test.run()
+        xenHvm64Test = XenTestRun(self.host, "test-hvm-64-example");
+        xenHvm64Test.run()

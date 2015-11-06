5213dee60d9cf5b29d8a17a4b48c5d6e767c2964
Anshul Makkar <anshul.makkar@citrix.com>
2015-11-06 14:31:43 +0000

    Xen-test-framework: Modify xenconsoled config file to log all guest logs in /var/log/xen.

--- exec/testcases/xenserver/tc/ring0/ring0xen.py
+++ exec/testcases/xenserver/tc/ring0/ring0xen.py
@@ -39,7 +39,9 @@ class TCRing0XenBase(xenrt.TestCase):
             sh.close()
 
         self.host.execdom0("rpm --force -Uvh %s" % (modulePath))
-        self.host.execdom0("xenconsoled --log=all")
+        self.host.execdom0("sed 's/#XENSTORED_TRACE.*/XENSTORED_TRACE=yes/' /etc/sysconfig/xencommons")
+        self.host.execdom0("sed 's/#XENCONSOLED_TRACE.*/XENCONSOLED_TRACE=all/' /etc/sysconfig/xencommons")
+        self.host.execdom0("/bin/systemctl restart xenconsoled.service")
 
 class TCRing0XenPV32Test(TCRing0XenBase):
     def run(self, arglist):

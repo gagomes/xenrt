8e347bb84a538f7b2d8e0430303f5f65c9e2b233
Alex Brett <alex.brett@citrix.com>
2015-11-12 17:03:09 +0000

    Only schedule a machines specified job if all the machines are in the same site
    At the moment you can submit a job specifying machines across sites - while they are all allocated to the job, those machines not in the same site as the first machine will not be useable by XenRT, thus leading to confusing results.
    
    This change therefore modifies the behaviour so we reject any specified machines in a different site to the first one.

--- server/app/api/schedule.py
+++ server/app/api/schedule.py
@@ -164,14 +164,22 @@ class XenRTSchedule(XenRTAPIPage):
                                 verbose.write("  wants %s, not available\n" % (mxs[0]))
                                 # unscheduable at the moment
                                 continue
-                            # Any remaining machines have site and cluster ignored
+                            # Any remaining machines have cluster ignored
                             schedulable = True
                             for mx in mxs[1:]:
                                 if len(selected) == machines_required:
                                     break
                                 if machines.has_key(mx) or leasedmachines.has_key(mx):
-                                    selected.append(mx)
-                                    verbose.write("  wants %s, it is available\n" % (mx))
+                                    if machines.has_key(mx):
+                                        m = machines[mx]
+                                    else:
+                                        m = leasedmachines[mx]
+                                    if m[1] != site:
+                                        verbose.write("  wants %s, but it's in a different site\n" % (mx))
+                                        schedulable = False
+                                    else:
+                                        selected.append(mx)
+                                        verbose.write("  wants %s, it is available\n" % (mx))
                                 else:
                                     verbose.write("  wants %s, not available\n" % (mx))
                                     # unscheduable at the moment

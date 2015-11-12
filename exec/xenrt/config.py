2ace934fcc234b12c3b8a02534216d4bfd5c28e7
Dan Lamping <daniel.lamping@citrix.com>
2015-11-12 10:26:46 +0000

    Fixed highmem issues

--- exec/xenrt/config.py
+++ exec/xenrt/config.py
@@ -1203,7 +1203,7 @@ class Config(object):
         self.config["VERSION_CONFIG"]["Dundee"]["TEMPLATE_NAME_SLED_12_64"] = "SUSE Linux Enterprise Desktop 12 (64-bit)"
         self.config["VERSION_CONFIG"]["Dundee"]["MAX_VBDS_PER_HOST"] = "4096"
         self.config["VERSION_CONFIG"]["Dundee"]["MAX_VDIS_PER_VM"] = "255"
-        self.config["VERSION_CONFIG"]["Dundee"]["MAX_MEM_HVM"] = "2097152"
+        self.config["VERSION_CONFIG"]["Dundee"]["MAX_MEM_HVM"] = "1572864"
 
         # XenServer dom0 partitions
         self.config["VERSION_CONFIG"]["Dundee"]["DOM0_PARTITIONS"] = {1:18*xenrt.GIGA, 2:18*xenrt.GIGA, 3:"*", 4:512*xenrt.MEGA, 5:4*xenrt.GIGA, 6:1024*xenrt.MEGA}

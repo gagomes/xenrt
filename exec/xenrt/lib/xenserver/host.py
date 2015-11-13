3cbe96c65394830d3192f3bdb49038c0e0fdd9fe
Vinod Pademar <vinod.pademar@citrix.com>
2015-11-13 15:20:54 +0000

    Simply looking for GenuineIntel or AuthenticAMD in /proc/cpuinfo

--- exec/xenrt/lib/xenserver/host.py
+++ exec/xenrt/lib/xenserver/host.py
@@ -3374,19 +3374,11 @@ fi
 
     def isSvmHardware(self):
         """Return True if host is HVM compatible and has SVM enabled"""
-        if isinstance(self, xenrt.lib.xenserver.BostonHost):
-            return self.isHvmEnabled() and "AuthenticAMD" in self.getHostParam("cpu_info")
-        else:
-            cpuinfo = self.execdom0("cat /proc/cpuinfo")
-            return self.isHvmEnabled() and re.search(r"AuthenticAMD", cpuinfo)
+        return self.isHvmEnabled() and re.search(r"AuthenticAMD", self.execdom0("cat /proc/cpuinfo"))
 
     def isVmxHardware(self):
         """Return True if host is HVM compatible and has VMX enabled"""
-        if isinstance(self, xenrt.lib.xenserver.BostonHost):
-            return self.isHvmEnabled() and "GenuineIntel" in self.getHostParam("cpu_info")
-        else:
-            cpuinfo = self.execdom0("cat /proc/cpuinfo")
-            return self.isHvmEnabled() and re.search(r"GenuineIntel", cpuinfo)
+        return self.isHvmEnabled() and re.search(r"GenuineIntel", self.execdom0("cat /proc/cpuinfo"))
 
     def getBridgeWithMapping(self, index):
         """Returns the name of a bridge corresponding to the interface
